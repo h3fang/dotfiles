4abcf3ea8d1e681bf9da446ce09267030a7f6d52
Fang He <hefun@qq.com>
2017-08-28 08:52:46 +0800

    switched to nvidia

--- switch_video_card.sh
+++ switch_video_card.sh
@@ -1,36 +1,36 @@
 #!/bin/sh
 
-use_nvidia()
+function use_nvidia
 {
-    cp /etc/X11/xorg.conf.nvidia /etc/X11/xorg.conf
+    sudo cp /etc/X11/xorg.conf.nvidia /etc/X11/xorg.conf
 
     # comment out poweroff nvidia card cmds
     HWFIXPath="/home/enigma/hwfix.sh"
-    sed -i '/^modprobe acpi_call/ s/^/#/' $HWFIXPath
-    sed -i '/^echo "\\_SB.PCI0.PEG0.PEGP._OFF" > \/proc\/acpi\/call/ s/^/#/' $HWFIXPath
-    sed -i '/^rmmod acpi_call/ s/^/#/' $HWFIXPath
-    
+    sudo sed -i '/^modprobe acpi_call/ s/^/#/' $HWFIXPath
+    sudo sed -i '/^echo "\\_SB.PCI0.PEG0.PEGP._OFF" > \/proc\/acpi\/call/ s/^/#/' $HWFIXPath
+    sudo sed -i '/^rmmod acpi_call/ s/^/#/' $HWFIXPath
+
     # remove blacklist nvidia card conf
-    mv /usr/lib/modprobe.d/graphicscard.conf /usr/lib/modprobe.d/graphicscard.conf.backup
+    sudo mv /usr/lib/modprobe.d/graphicscard.conf /usr/lib/modprobe.d/graphicscard.conf.backup
 
     # enable PRIME
-    sed -i '/^#xrandr --setprovideroutputsource modesetting NVIDIA-0/ s/^#//' /etc/lightdm/display_setup.sh
+    sudo sed -i '/^#xrandr --setprovideroutputsource modesetting NVIDIA-0/ s/^#//' /etc/lightdm/display_setup.sh
 }
 
-use_intel()
+function use_intel
 {
-    cp /etc/X11/xorg.conf.intel /etc/X11/xorg.conf
+    sudo cp /etc/X11/xorg.conf.intel /etc/X11/xorg.conf
 
     # poweroff nvidia card
     HWFIXPath="/home/enigma/hwfix.sh"
-    sed -i '/^#modprobe acpi_call/ s/^#//' $HWFIXPath
-    sed -i '/^#echo "\\_SB.PCI0.PEG0.PEGP._OFF" > \/proc\/acpi\/call/ s/^#//' $HWFIXPath
-    sed -i '/^#rmmod acpi_call/ s/^#//' $HWFIXPath
+    sudo sed -i '/^#modprobe acpi_call/ s/^#//' $HWFIXPath
+    sudo sed -i '/^#echo "\\_SB.PCI0.PEG0.PEGP._OFF" > \/proc\/acpi\/call/ s/^#//' $HWFIXPath
+    sudo sed -i '/^#rmmod acpi_call/ s/^#//' $HWFIXPath
 
     # blacklist nvidia card
-    mv /usr/lib/modprobe.d/graphicscard.conf.backup /usr/lib/modprobe.d/graphicscard.conf
+    sudo mv /usr/lib/modprobe.d/graphicscard.conf.backup /usr/lib/modprobe.d/graphicscard.conf
 
     # disable PRIME
-    sed -i '/^xrandr --setprovideroutputsource modesetting NVIDIA-0/ s/^/#/' /etc/lightdm/display_setup.sh
+    sudo sed -i '/^xrandr --setprovideroutputsource modesetting NVIDIA-0/ s/^/#/' /etc/lightdm/display_setup.sh
 }
 
