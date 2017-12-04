1fee0c97e30f0d21a023c2f2e093edf4f87fa7c0
Fang He <hefun@qq.com>
2017-12-04 21:35:47 +0800

    remove sound card fix

--- hwfix.sh
+++ hwfix.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-hda-verb /dev/snd/hwC0D0 0x20 SET_COEF_INDEX 0x67
-hda-verb /dev/snd/hwC0D0 0x20 SET_PROC_COEF 0x3000
+#hda-verb /dev/snd/hwC0D0 0x20 SET_COEF_INDEX 0x67
+#hda-verb /dev/snd/hwC0D0 0x20 SET_PROC_COEF 0x3000
 
 #modprobe acpi_call
 #echo "\_SB.PCI0.PEG0.PEGP._OFF" > /proc/acpi/call
