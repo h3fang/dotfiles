38d622ef294b6ae6592f83f670872f6de84bd5cc
Fang He <hefun@qq.com>
2018-02-02 23:39:14 +0800

    add backup script

---
+++ scripts/backup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+tar --exclude='/home/enigma/.config/Atom' --exclude='/home/enigma/.config/chromium' --exclude='/home/enigma/projects/ns3/src/ns-allinone-3.27/ns-3.27/src' --exclude='/home/enigma/projects/ns3/src/ns-allinone-3.27/ns-3.27/build' --exclude='/home/enigma/projects/ns3/ns*' --exclude='/home/enigma/projects/ns3/src/ns3*' -zcvf arch-enigma-home.tar.gz ~/.config ~/projects ~/scripts
