53ca52569f9e4d617b6faeec3b477f363ecdde9b
Fang He <hefun@qq.com>
2018-02-19 23:29:52 +0800

    fix hwfix.sh path

--- scripts/switch_video_card.sh
+++ scripts/switch_video_card.sh
@@ -3,9 +3,10 @@
 function use_nvidia
 {
     sudo cp /etc/X11/xorg.conf.nvidia /etc/X11/xorg.conf
+    sudo mv /usr/share/X11/xorg.conf.d/10-nvidia-drm-outputclass.conf /usr/share/X11/xorg.conf.d/10-nvidia-drm-outputclass.conf.backup
 
     # comment out poweroff nvidia card cmds
-    HWFIXPath="/home/enigma/hwfix.sh"
+    HWFIXPath="/home/enigma/scripts/hwfix.sh"
     sudo sed -i '/^modprobe acpi_call/ s/^/#/' $HWFIXPath
     sudo sed -i '/^echo "\\_SB.PCI0.PEG0.PEGP._OFF" > \/proc\/acpi\/call/ s/^/#/' $HWFIXPath
     sudo sed -i '/^rmmod acpi_call/ s/^/#/' $HWFIXPath
@@ -20,9 +21,10 @@ function use_nvidia
 function use_intel
 {
     sudo cp /etc/X11/xorg.conf.intel /etc/X11/xorg.conf
+    sudo mv /usr/share/X11/xorg.conf.d/10-nvidia-drm-outputclass.conf /usr/share/X11/xorg.conf.d/10-nvidia-drm-outputclass.conf.backup
 
     # poweroff nvidia card
-    HWFIXPath="/home/enigma/hwfix.sh"
+    HWFIXPath="/home/enigma/scripts/hwfix.sh"
     sudo sed -i '/^#modprobe acpi_call/ s/^#//' $HWFIXPath
     sudo sed -i '/^#echo "\\_SB.PCI0.PEG0.PEGP._OFF" > \/proc\/acpi\/call/ s/^#//' $HWFIXPath
     sudo sed -i '/^#rmmod acpi_call/ s/^#//' $HWFIXPath
