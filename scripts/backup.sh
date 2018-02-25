434f8d7c3e3cfab02fed2798be1c1bfe55268364
Fang He <hefun@qq.com>
2018-02-25 21:50:35 +0800

    fix backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-tar --exclude='/home/enigma/.config/Atom' --exclude='/home/enigma/.config/chromium' --exclude='/home/enigma/projects/ns3/src/ns-allinone-3.27/ns-3.27/src' --exclude='/home/enigma/projects/ns3/src/ns-allinone-3.27/ns-3.27/build' --exclude='/home/enigma/projects/ns3/src/ns-allinone-3.27/ns-3.27/src' --exclude='/home/enigma/projects/ns3/ns*' --exclude='/home/enigma/projects/ns3/pkg' --exclude='/home/enigma/projects/ns3/src/ns3*' -zcvf arch-enigma-home-$(date -I).tar.gz ~/.config ~/projects ~/scripts
+tar --exclude='/home/enigma/.config/Atom' --exclude='/home/enigma/.config/chromium' --exclude='/home/enigma/projects/ns3/src' --exclude='/home/enigma/projects/ns3/ns*' --exclude='/home/enigma/projects/ns3/pkg' -zcvf arch-enigma-home-$(date -I).tar.gz ~/.config ~/projects ~/scripts
 
