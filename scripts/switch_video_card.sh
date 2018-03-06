95d445e11c6bd95295fb623be4305aebf0d284b8
Fang He <hefun@qq.com>
2018-03-06 19:02:45 +0800

    add use_nouveau

--- scripts/switch_video_card.sh
+++ scripts/switch_video_card.sh
@@ -10,7 +10,7 @@ function use_nvidia
         sudo mv $outputclass.backup $outputclass
     fi
 
-    # comment out poweroff nvidia card cmds
+    # do not poweroff nvidia GPU
     HWFIXPath="/home/enigma/scripts/hwfix.sh"
     sudo sed -i '/^modprobe acpi_call/ s/^/#/' $HWFIXPath
     sudo sed -i '/^echo "\\_SB.PCI0.PEG0.PEGP._OFF" > \/proc\/acpi\/call/ s/^/#/' $HWFIXPath
@@ -19,7 +19,9 @@ function use_nvidia
     # remove blacklist nvidia card conf
     sudo mv /usr/lib/modprobe.d/graphicscard.conf /usr/lib/modprobe.d/graphicscard.conf.backup
 
-    # enable PRIME
+    # comment out all PRIME settings
+    sudo sed -i '/^xrandr --set/ s/^/#/' /etc/lightdm/display_setup.sh
+    # enable reverse PRIME for nvidia
     sudo sed -i '/^#xrandr --setprovideroutputsource modesetting NVIDIA-0/ s/^#//' /etc/lightdm/display_setup.sh
 }
 
@@ -33,16 +35,43 @@ function use_intel
         sudo mv $outputclass $outputclass.backup
     fi
 
-    # poweroff nvidia card
+    # poweroff nvidia GPU
     HWFIXPath="/home/enigma/scripts/hwfix.sh"
     sudo sed -i '/^#modprobe acpi_call/ s/^#//' $HWFIXPath
     sudo sed -i '/^#echo "\\_SB.PCI0.PEG0.PEGP._OFF" > \/proc\/acpi\/call/ s/^#//' $HWFIXPath
     sudo sed -i '/^#rmmod acpi_call/ s/^#//' $HWFIXPath
 
-    # blacklist nvidia card
+    # blacklist nvidia
     sudo mv /usr/lib/modprobe.d/graphicscard.conf.backup /usr/lib/modprobe.d/graphicscard.conf
 
     # disable PRIME
-    sudo sed -i '/^xrandr --setprovideroutputsource modesetting NVIDIA-0/ s/^/#/' /etc/lightdm/display_setup.sh
+    sudo sed -i '/^xrandr --set/ s/^/#/' /etc/lightdm/display_setup.sh
+}
+
+function use_nouveau
+{
+    sudo cp /etc/X11/xorg.conf.nouveau /etc/X11/xorg.conf
+
+    outputclass="/usr/share/X11/xorg.conf.d/10-nvidia-drm-outputclass.conf"
+    if [ -e $outputclass ]
+    then
+        sudo mv $outputclass $outputclass.backup
+    fi
+
+    # do not poweroff nvidia GPU
+    HWFIXPath="/home/enigma/scripts/hwfix.sh"
+    sudo sed -i '/^modprobe acpi_call/ s/^/#/' $HWFIXPath
+    sudo sed -i '/^echo "\\_SB.PCI0.PEG0.PEGP._OFF" > \/proc\/acpi\/call/ s/^/#/' $HWFIXPath
+    sudo sed -i '/^rmmod acpi_call/ s/^/#/' $HWFIXPath
+
+    # blacklist nvidia
+    sudo mv /usr/lib/modprobe.d/graphicscard.conf.backup /usr/lib/modprobe.d/graphicscard.conf
+    # un-blacklist nouveau
+    sudo mv /usr/lib/modprobe.d/nvidia.conf /usr/lib/modprobe.d/nvidia.conf.backup
+
+    # comment out all PRIME settings
+    sudo sed -i '/^xrandr --set/ s/^/#/' /etc/lightdm/display_setup.sh
+    # enable reverse PRIME for nouveau
+    sudo sed -i '/^#xrandr --setprovideroutputsource modesetting nouveau/ s/^#//' /etc/lightdm/display_setup.sh
 }
 
