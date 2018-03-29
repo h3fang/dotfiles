6a888ae3becce4af4a0bf877fe6af3197e52c978
Fang He <hefun@qq.com>
2018-03-29 13:01:03 +0800

    exclude mpv/watch_later folder from backup

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-tar --exclude='/home/enigma/.config/Atom' --exclude='/home/enigma/.config/chromium' --exclude='/home/enigma/projects/ns3/src' --exclude='/home/enigma/projects/ns3/ns*' --exclude='/home/enigma/projects/ns3/pkg' -zcvf arch-enigma-home-$(date -I).tar.gz ~/.config ~/projects ~/scripts
+tar --exclude='/home/enigma/.config/mpv/watch_later/' --exclude='/home/enigma/.config/Atom' --exclude='/home/enigma/.config/chromium' --exclude='/home/enigma/projects/ns3/src' --exclude='/home/enigma/projects/ns3/ns*' --exclude='/home/enigma/projects/ns3/pkg' -zcvf arch-enigma-home-$(date -I).tar.gz ~/.config ~/projects ~/scripts
 
