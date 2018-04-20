e729418d3bcf2051dba2f8620fde3f85e8820e71
Fang He <hefun@qq.com>
2018-04-20 12:07:40 +0800

    update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -7,7 +7,13 @@ tar --exclude='/home/enigma/.config/mpv/watch_later' \
     --exclude='/home/enigma/projects/ns3/src' \
     --exclude='/home/enigma/projects/ns3/ns*' \
     --exclude='/home/enigma/projects/ns3/pkg' \
+    --exclude='/home/enigma/projects/ClusterAD/Data' \
     -zcvf arch-enigma-home-$(date -I).tar.gz \
     ~/.config \
     ~/projects \
-    ~/scripts
+    ~/scripts \
+    ~/.gtkrc-2.0 \
+    ~/.gitconfig \
+    ~/.bashrc \
+    ~/.vimrc \
+    ~/.gitignore
