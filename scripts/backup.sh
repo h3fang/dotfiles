52df401a8836e5dd94b17f4af41f1d4e39c11fb4
Fang He <hefun@qq.com>
2018-05-03 10:07:46 +0800

    use pigz in backup.sh for parallel compression

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-tar --exclude='/home/enigma/.config/mpv/watch_later' \
+tar -I pigz \
+    --exclude='/home/enigma/.config/mpv/watch_later' \
     --exclude='/home/enigma/.config/Atom' \
     --exclude='/home/enigma/.config/chromium' \
     --exclude='/home/enigma/.config/Code' \
@@ -8,7 +9,7 @@ tar --exclude='/home/enigma/.config/mpv/watch_later' \
     --exclude='/home/enigma/projects/ns3/ns*' \
     --exclude='/home/enigma/projects/ns3/pkg' \
     --exclude='/home/enigma/projects/ClusterAD/Data' \
-    -zcvf arch-enigma-home-$(date -I).tar.gz \
+    -cvf arch-enigma-home-$(date -I).tar.gz \
     ~/.config \
     ~/projects \
     ~/scripts \
