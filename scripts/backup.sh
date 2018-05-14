0222b3276fb470dda1c0a4ea9120504d96200ab6
Fang He <hefun@qq.com>
2018-05-14 14:39:47 +0800

    backup Documents and Pictures folder

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -8,10 +8,13 @@ tar -I pigz --exclude='.config/mpv/watch_later' \
     --exclude='projects/ns3/ns*' \
     --exclude='projects/ns3/pkg' \
     --exclude='projects/ClusterAD/Data' \
+    --exclude='.~lock.*' \
     -cvf arch-enigma-home-$(date -I).tar.gz \
     ~/.config \
     ~/projects \
     ~/scripts \
+    ~/Pictures \
+    ~/Documents \
     ~/.gtkrc-2.0 \
     ~/.gitconfig \
     ~/.bashrc \
