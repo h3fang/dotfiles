3570ef8ba1f7755b2fe88e4f048b1f7121bd7858
Fang He <hefun@qq.com>
2018-05-26 17:26:42 +0800

    update installed packages

--- scripts/install_arch.sh
+++ scripts/install_arch.sh
@@ -11,5 +11,5 @@ cd pikaur
 makepkg -fsri
 
 # install blablabla
-sudo pikaur -S htop networkmanager ttf-dejavu ttf-liberation ttf-hack ttf-font-awesome font-mathematica noto-fonts-cjk noto-fonts-emoji ttf-symbola gvim chromium dhclient openssh lightdm lightdm-gtk-greeter numlockx gedit evince xfce4-terminal thunar gvfs thunar-volman rofi compton feh xorg-server xorg-xrandr mesa scrot fuseiso zip unzip unrar p7zip file-roller ntfs-3g poppler-data xss-lock-git brightnessctl ppp transmission-qt youtube-dl pulseaudio mpv gimp atom valgrind python python-pip qt qtcreator gdb cmake texlive-most texlive-langchinese texlive-langgreek biber zotero wd719x-firmware aic94xx-firmware openblas-lapack
+sudo pikaur -S htop networkmanager ttf-dejavu ttf-liberation ttf-hack otf-font-awesome font-mathematica noto-fonts-cjk noto-fonts-emoji ttf-symbola gvim chromium dhclient openssh lightdm lightdm-gtk-greeter numlockx mousepad evince xfce4-terminal thunar gvfs thunar-volman rofi compton feh xorg-server xorg-xrandr mesa maim fuseiso zip unzip unrar p7zip file-roller ntfs-3g poppler-data xss-lock-git brightnessctl ppp transmission-qt youtube-dl pulseaudio mpv gimp valgrind python python-pip qt qtcreator gdb cmake texlive-most texlive-langchinese texlive-langgreek biber zotero openblas-lapack visual-studio-code-bin
 
