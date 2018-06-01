75b7a4e87c5dd6eec7e12c0431925e5c116493c4
Fang He <hefun@qq.com>
2018-06-01 21:22:48 +0800

    update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -8,6 +8,7 @@ tar -I pigz --exclude='.config/mpv/watch_later' \
     --exclude='projects/ns3/ns*' \
     --exclude='projects/ns3/pkg' \
     --exclude='projects/ClusterAD/Data' \
+    --exclude='projects/Southwest-Weight-Estimation/Data' \
     --exclude='.~lock.*' \
     -cvf arch-enigma-home-$(date -I).tar.gz \
     ~/.config \
