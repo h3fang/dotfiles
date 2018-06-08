15c860d85fec52c046e232d367d558ddc2bf2557
Fang He <hefun@qq.com>
2018-06-08 16:51:25 +0800

    refine backup_hd.sh

--- scripts/backup_hd.sh
+++ scripts/backup_hd.sh
@@ -1,2 +1,7 @@
-rsync -ahv --exclude='MATLAB' --exclude='.cache' --exclude='projects/*/Data' ~ /run/media/enigma/stash/backup/
+# no space after comma
+# no slash after source directory
+rsync -ahv --delete --exclude={"MATLAB",".cache","projects/*/Data",".local/share/Trash",".thumbnails",".gvfs"} ~ /run/media/enigma/stash/backup/
+rsync -ahv --delete /etc /run/media/enigma/stash/backup/
+rsync -ahv --delete /usr/lib/modprobe.d /run/media/enigma/stash/backup/
+rsync -ahv --delete /usr/lib/modules-load.d /run/media/enigma/stash/backup/
 
