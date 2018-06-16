8714e7c1bed9bc61873ec6108608b493c1a91dbe
Fang He <hefun@qq.com>
2018-06-16 10:38:24 +0800

    add windows backup script

---
+++ scripts/backup_win.sh
@@ -0,0 +1,6 @@
+# no space after comma
+# no slash after source directory
+rsync -ahv --delete /run/media/enigma/Data /run/media/enigma/stash/backup/win_Data/
+rsync -ahv --delete /run/media/enigma/Windows/Users/hfeni/Downloads /run/media/enigma/stash/backup/win_Data/
+rsync -ahv --delete /run/media/enigma/Windows/Users/hfeni/Pictures /run/media/enigma/stash/backup/win_Data/
+
