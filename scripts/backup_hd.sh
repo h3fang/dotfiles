9af7dd6cbb5efe4cb945ac61d30041f7b0ae530f
Fang He <hefun@qq.com>
2018-06-17 15:10:01 +0800

    ignore cache dirs in backup script

--- scripts/backup_hd.sh
+++ scripts/backup_hd.sh
@@ -1,6 +1,6 @@
 # no space after comma
 # no slash after source directory
-rsync -ahv --delete --exclude={"MATLAB",".cache","projects/*/Data",".local/share/Trash",".thumbnails",".gvfs"} ~ /run/media/enigma/stash/backup/
+rsync -ahv --delete --exclude={"MATLAB",".cache","projects/*/Data",".local/share/Trash",".nv",".thumbnails",".gvfs",".config/Code/*Cache*",".config/chromium/**/*Cache*"} ~ /run/media/enigma/stash/backup/
 rsync -ahv --delete /etc /run/media/enigma/stash/backup/
 rsync -ahv --delete /usr/lib/modprobe.d /run/media/enigma/stash/backup/
 rsync -ahv --delete /usr/lib/modules-load.d /run/media/enigma/stash/backup/
