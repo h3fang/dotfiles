1944a24ea45c97b41073c0222ce4b2f6c80922e5
Fang He <hefun@qq.com>
2018-06-20 10:26:37 +0800

    fix scripts/backup_win.sh

--- scripts/backup_win.sh
+++ scripts/backup_win.sh
@@ -1,6 +1,7 @@
 # no space after comma
 # no slash after source directory
-rsync -ahv --delete /run/media/enigma/Data /run/media/enigma/stash/backup/win_Data/
-rsync -ahv --delete /run/media/enigma/Windows/Users/hfeni/Downloads /run/media/enigma/stash/backup/win_Data/
-rsync -ahv --delete /run/media/enigma/Windows/Users/hfeni/Pictures /run/media/enigma/stash/backup/win_Data/
+rsync -ahv --delete --ignore-errors --delete-excluded --exclude={"\$RECYCLE.BIN","System Volume Information"} /run/media/enigma/Data /run/media/enigma/stash/backup/win_Data/
+rsync -ahv --delete --ignore-errors --delete-excluded /run/media/enigma/Windows/Users/hfeni/Downloads /run/media/enigma/stash/backup/win_Data/
+rsync -ahv --delete --ignore-errors --delete-excluded /run/media/enigma/Windows/Users/hfeni/Pictures /run/media/enigma/stash/backup/win_Data/
+rsync -ahv --delete --ignore-errors --delete-excluded /run/media/enigma/Windows/Users/hfeni/Documents/WeChat\ Files /run/media/enigma/stash/backup/win_Data/
 
