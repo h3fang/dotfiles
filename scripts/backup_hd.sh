05e7dfb09c19a2fe462c2e516f86930007e866c3
Fang He <hefun@qq.com>
2018-06-20 17:29:44 +0800

    add /boot/loader/ to backup_hd.sh

--- scripts/backup_hd.sh
+++ scripts/backup_hd.sh
@@ -2,6 +2,7 @@
 # no slash after source directory
 rsync -ahv --delete --ignore-errors --delete-excluded --exclude={"MATLAB",".cache","projects/*/Data",".local/share/Trash",".nv",".thumbnails",".gvfs",".config/Code/*Cache*",".config/chromium/**/*Cache*",".config/Code/logs",".local/share/gvfs-metadata",".local/share/recently-used.xbel*","arch-enigma-home-*.tar.gz"} ~ /run/media/enigma/stash/backup/
 rsync -ahv --delete --ignore-errors --delete-excluded /etc /run/media/enigma/stash/backup/
+rsync -ahv --delete --ignore-errors --delete-excluded /boot/loader /run/media/enigma/stash/backup/
 rsync -ahv --delete --ignore-errors --delete-excluded /usr/lib/modprobe.d /run/media/enigma/stash/backup/
 rsync -ahv --delete --ignore-errors --delete-excluded /usr/lib/modules-load.d /run/media/enigma/stash/backup/
 
