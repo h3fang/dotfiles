0cc2f4a2776b624add94fa9f371fd161e0b7ae6d
Fang He <hefun@qq.com>
2018-06-27 20:34:37 +0800

    add mount instructions for .cache and .config folders

--- scripts/hwfix.sh
+++ scripts/hwfix.sh
@@ -3,7 +3,10 @@
 #hda-verb /dev/snd/hwC0D0 0x20 SET_COEF_INDEX 0x67
 #hda-verb /dev/snd/hwC0D0 0x20 SET_PROC_COEF 0x3000
 
-#modprobe acpi_call
-#echo "\_SB.PCI0.PEG0.PEGP._OFF" > /proc/acpi/call
-#rmmod acpi_call
+mount --bind /mnt/cache/.cache /home/enigma/.cache
+mount --bind /mnt/cache/.config /home/enigma/.config
+
+modprobe acpi_call
+echo "\_SB.PCI0.PEG0.PEGP._OFF" > /proc/acpi/call
+rmmod acpi_call
 
