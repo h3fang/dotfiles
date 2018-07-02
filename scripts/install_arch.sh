b8669ccbc5c791e63b92710ed0dccfb2027e99e9
Fang He <hefun@qq.com>
2018-07-02 13:34:23 +0800

    update install script

--- scripts/install_arch.sh
+++ scripts/install_arch.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-sudo pacman -Syyu
+sudo pacman -Syu
 
 sudo pacman -S base-devel git 
 
 # install pikaur
 cd ~/Downloads
-git clone https://aur.archlinux.org/pikaur.git
-cd pikaur
+git clone https://aur.archlinux.org/aurman.git
+cd aurman
 makepkg -fsri
 
-# install blablabla
-sudo pikaur -S htop networkmanager ttf-dejavu ttf-liberation ttf-hack otf-font-awesome font-mathematica noto-fonts-cjk noto-fonts-emoji ttf-symbola gvim chromium dhclient openssh lightdm lightdm-gtk-greeter numlockx mousepad evince xfce4-terminal thunar gvfs thunar-volman rofi compton feh xorg-server xorg-xrandr mesa maim fuseiso zip unzip unrar p7zip file-roller ntfs-3g poppler-data xss-lock-git brightnessctl ppp transmission-qt youtube-dl pulseaudio mpv gimp valgrind python python-pip qt qtcreator gdb cmake texlive-most texlive-langchinese texlive-langgreek biber zotero openblas-lapack visual-studio-code-bin
+# fetch dotfiles
+cd ~/Downloads
+git clone git@github.com:h3fang/dotfiles.git
+mv dotfiles/{,.[^.]}* ~
+
+# install others
+sudo aurman -S htop networkmanager ttf-dejavu ttf-liberation ttf-hack otf-font-awesome font-mathematica noto-fonts-cjk noto-fonts-emoji gvim chromium dhclient openssh lightdm lightdm-gtk-greeter numlockx mousepad evince xfce4-terminal thunar gvfs thunar-volman rofi compton feh xorg-server xorg-xrandr xorg-xinput xorg-xset mesa maim fuseiso zip unzip unrar p7zip file-roller ntfs-3g poppler-data xss-lock-git brightnessctl ppp transmission-qt youtube-dl pulseaudio mpv gimp valgrind python python-pip qt qtcreator gdb cmake texlive-most texlive-langchinese biber zotero openblas-lapack visual-studio-code-bin
 
