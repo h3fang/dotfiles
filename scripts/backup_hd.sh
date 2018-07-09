80cd025c5204fc384b65ba5a100fd04ccc8ff923
Fang He <hefun@qq.com>
2018-07-09 13:16:52 +0800

    update backup scripts

--- scripts/backup_hd.sh
+++ scripts/backup_hd.sh
@@ -1,6 +1,6 @@
 # no space after comma
 # no slash after source directory
-rsync -ahv --delete --ignore-errors --delete-excluded --exclude={"MATLAB",".cache","projects/*/Data",".local/share/Trash",".nv",".thumbnails",".gvfs",".config/Code/*Cache*",".config/chromium",".config/Code/logs",".local/share/gvfs-metadata",".local/share/recently-used.xbel*","arch-enigma-home-*.tar.gz",".vscode/extensions"} ~ /run/media/enigma/stash/backup/
+rsync -ahv --delete --ignore-errors --delete-excluded --exclude={"MATLAB",".cache","projects/*/Data","projects/AUR/*/*/*","projects/AUR/*/*.tar.xz",".local/share/Trash",".nv",".thumbnails",".gvfs",".config/Code/*Cache*",".config/chromium",".config/Code/logs",".local/share/gvfs-metadata",".local/share/recently-used.xbel*","arch-enigma-home-*.tar.gz",".vscode/extensions"} ~ /run/media/enigma/stash/backup/
 rsync -ahv --delete --ignore-errors --delete-excluded /etc /run/media/enigma/stash/backup/
 rsync -ahv --delete --ignore-errors --delete-excluded /boot/loader /run/media/enigma/stash/backup/
 rsync -ahv --delete --ignore-errors --delete-excluded /usr/lib/modprobe.d /run/media/enigma/stash/backup/
--- scripts/backup_zip.sh
+++ scripts/backup_zip.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-tar -I pigz --exclude='.config/mpv/watch_later' \
+tar -I pigz \
+    --exclude='.config/mpv/watch_later' \
     --exclude='.config/Atom' \
     --exclude='.config/chromium' \
     --exclude='.config/Code' \
     --exclude='projects/**/build' \
     --exclude='projects/**/Data' \
     --exclude='projects/**/Results' \
+    --exclude="projects/AUR/*/*.tar.xz" \
+    --exclude='projects/AUR/*/*/*' \
     --exclude='.~lock.*' \
     -cvf arch-enigma-home-$(date -I).tar.gz \
     ~/.config \
