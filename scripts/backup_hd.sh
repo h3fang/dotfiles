b82a7ddb6539960db061aa10aa39437058f05c61
Fang He <hefun@qq.com>
2018-07-15 13:38:42 +0800

    update backup scripts for devmon

--- scripts/backup_hd.sh
+++ scripts/backup_hd.sh
@@ -1,8 +1,8 @@
 # no space after comma
 # no slash after source directory
-rsync -ahv --delete --ignore-errors --delete-excluded --exclude={"MATLAB",".cache","projects/*/Data","projects/AUR/*/*/*","projects/AUR/*/*.tar.?z",".local/share/Trash",".nv",".thumbnails",".gvfs",".config/Code/*Cache*",".config/chromium",".config/Code/logs",".local/share/gvfs-metadata",".local/share/recently-used.xbel*","arch-enigma-home-*.tar.gz",".vscode/extensions"} ~ /run/media/enigma/stash/backup/
-rsync -ahv --delete --ignore-errors --delete-excluded /etc /run/media/enigma/stash/backup/
-rsync -ahv --delete --ignore-errors --delete-excluded /boot/loader /run/media/enigma/stash/backup/
-rsync -ahv --delete --ignore-errors --delete-excluded /usr/lib/modprobe.d /run/media/enigma/stash/backup/
-rsync -ahv --delete --ignore-errors --delete-excluded /usr/lib/modules-load.d /run/media/enigma/stash/backup/
+rsync -ahv --delete --ignore-errors --delete-excluded --exclude={"MATLAB",".cache","projects/*/Data","projects/AUR/*/*/*","projects/AUR/*/*.tar.?z",".local/share/Trash",".nv",".thumbnails",".gvfs",".config/Code/*Cache*",".config/chromium",".config/Code/logs",".local/share/gvfs-metadata",".local/share/recently-used.xbel*","arch-enigma-home-*.tar.gz",".vscode/extensions"} ~ /media/stash/backup/
+rsync -ahv --delete --ignore-errors --delete-excluded /etc /media/stash/backup/
+rsync -ahv --delete --ignore-errors --delete-excluded /boot/loader /media/stash/backup/
+rsync -ahv --delete --ignore-errors --delete-excluded /usr/lib/modprobe.d /media/stash/backup/
+rsync -ahv --delete --ignore-errors --delete-excluded /usr/lib/modules-load.d /media/stash/backup/
 
--- scripts/backup_win.sh
+++ scripts/backup_win.sh
@@ -1,7 +1,7 @@
 # no space after comma
 # no slash after source directory
-rsync -ahv --delete --ignore-errors --delete-excluded --exclude={"\$RECYCLE.BIN","System Volume Information"} /run/media/enigma/Data /run/media/enigma/stash/backup/win_Data/
-rsync -ahv --delete --ignore-errors --delete-excluded /run/media/enigma/Windows/Users/hfeni/Downloads /run/media/enigma/stash/backup/win_Data/
-rsync -ahv --delete --ignore-errors --delete-excluded /run/media/enigma/Windows/Users/hfeni/Pictures /run/media/enigma/stash/backup/win_Data/
-rsync -ahv --delete --ignore-errors --delete-excluded /run/media/enigma/Windows/Users/hfeni/Documents/WeChat\ Files /run/media/enigma/stash/backup/win_Data/
+rsync -ahv --delete --ignore-errors --delete-excluded --exclude={"\$RECYCLE.BIN","System Volume Information"} /run/media/enigma/Data /media/stash/backup/win_Data/
+rsync -ahv --delete --ignore-errors --delete-excluded /run/media/enigma/Windows/Users/hfeni/Downloads /media/stash/backup/win_Data/
+rsync -ahv --delete --ignore-errors --delete-excluded /run/media/enigma/Windows/Users/hfeni/Pictures /media/stash/backup/win_Data/
+rsync -ahv --delete --ignore-errors --delete-excluded /run/media/enigma/Windows/Users/hfeni/Documents/WeChat\ Files /media/stash/backup/win_Data/
 
