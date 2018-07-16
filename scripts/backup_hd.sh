d29faab9463cb917259dc86d4ab0144ffd8c63fb
Fang He <hefun@qq.com>
2018-07-16 12:06:15 +0800

    refine backup script

--- scripts/backup_hd.sh
+++ scripts/backup_hd.sh
@@ -1,8 +1,14 @@
+#!/bin/sh
+
 # no space after comma
 # no slash after source directory
 rsync -ahv --delete --ignore-errors --delete-excluded --exclude={"MATLAB",".cache","projects/*/Data","projects/AUR/*/*/*","projects/AUR/*/*.tar.?z",".local/share/Trash",".nv",".thumbnails",".gvfs",".config/Code/*Cache*",".config/chromium",".config/Code/logs",".local/share/gvfs-metadata",".local/share/recently-used.xbel*","arch-enigma-home-*.tar.gz",".vscode/extensions"} ~ /media/stash/backup/
-rsync -ahv --delete --ignore-errors --delete-excluded /etc /media/stash/backup/
-rsync -ahv --delete --ignore-errors --delete-excluded /boot/loader /media/stash/backup/
-rsync -ahv --delete --ignore-errors --delete-excluded /usr/lib/modprobe.d /media/stash/backup/
-rsync -ahv --delete --ignore-errors --delete-excluded /usr/lib/modules-load.d /media/stash/backup/
+#rsync -ahv --delete --ignore-errors --delete-excluded /etc /media/stash/backup/
+#rsync -ahv --delete --ignore-errors --delete-excluded /boot/loader /media/stash/backup/
+#rsync -ahv --delete --ignore-errors --delete-excluded /usr/lib/modprobe.d /media/stash/backup/
+#rsync -ahv --delete --ignore-errors --delete-excluded /usr/lib/modules-load.d /media/stash/backup/
+
+for target in /etc /boot/loader /usr/lib/modprobe.d /usr/lib/modules-load.d ; do
+    rsync -ahv --delete --ignore-errors --delete-excluded $target /media/stash/backup/
+done
 
