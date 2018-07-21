28312aab491747f7388f1d6789fb22b9faf77b1f
Fang He <hefun@qq.com>
2018-07-21 20:53:58 +0800

    fix devmon error

--- scripts/safely_remove_external_hdd.sh
+++ scripts/safely_remove_external_hdd.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$#" -eq 1 ] ; then
-    sync && devmon --unmount /dev/$1 && sudo hdparm -Y /dev/$1
+    sync && devmon -r && sudo hdparm -Y /dev/$1
 else
     echo "Error: need one argument for target device"
 fi
