c3bc5dca44966f842e7197c0d1226678d905833f
Fang He <hefun@qq.com>
2018-07-26 15:30:25 +0800

    remove removable storage before exit i3; check GUI apps are closed

--- .config/i3/scripts/close_apps.sh
+++ .config/i3/scripts/close_apps.sh
@@ -4,3 +4,11 @@ pkill transmission-qt
 i3-msg [class="."] kill
 sleep 3
 
+## gracefully close all apps, needs wmctrl
+# wmctrl -l | awk '{print $1}' | while read APP; do
+#   notify-send "$APP not closed, please check unsaved content and close it manually."
+#   exit 1
+# done
+
+devmon -r
+
