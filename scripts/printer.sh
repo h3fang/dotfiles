a2a1821182ae8fec1b2bb07f5b706c432626554c
Fang He <hefun@qq.com>
2018-07-28 22:30:02 +0800

    add printer setup script

---
+++ scripts/printer.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# hplip is for HP printers, see https://wiki.archlinux.org/index.php/CUPS/Printer-specific_problems
+sudo pacman -S cups hplip
+sudo systemctl enable org.cups.cupsd.service
+sudo systemctl start org.cups.cupsd.service
+
+# open http://localhost:631/ in web browser
+# 1) add current user to groups with printer administration privileges defined in SystemGroup in the /etc/cups/cups-files.conf or 2) login as root
+# add printer
+# print
+
