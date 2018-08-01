563b44b724de1597f3f746e563444b9f39b078d9
Fang He <hefun@qq.com>
2018-08-01 20:41:40 +0800

    update install script

--- scripts/install_arch.sh
+++ scripts/install_arch.sh
@@ -4,7 +4,7 @@ sudo pacman -Syu
 
 sudo pacman -S base-devel git 
 
-# install pikaur
+# install AUR helper
 cd ~/Downloads
 git clone https://aur.archlinux.org/aurman.git
 cd aurman
@@ -13,8 +13,9 @@ makepkg -fsri
 # fetch dotfiles
 cd ~/Downloads
 git clone git@github.com:h3fang/dotfiles.git
-mv dotfiles/{,.[^.]}* ~
+mv dotfiles/* ~
+rm -rf dotfiles/
 
 # install others
-sudo aurman -S htop networkmanager ttf-dejavu ttf-liberation ttf-hack otf-font-awesome font-mathematica noto-fonts-cjk noto-fonts-emoji gvim chromium dhclient openssh lightdm lightdm-gtk-greeter numlockx mousepad evince xfce4-terminal thunar gvfs thunar-volman rofi compton feh xorg-server xorg-xrandr xorg-xinput xorg-xset mesa maim fuseiso zip unzip unrar p7zip file-roller ntfs-3g poppler-data xss-lock-git brightnessctl ppp transmission-qt youtube-dl pulseaudio mpv gimp valgrind python python-pip qt qtcreator gdb cmake texlive-most texlive-langchinese biber zotero openblas-lapack visual-studio-code-bin
+sudo aurman -S htop networkmanager ttf-dejavu ttf-liberation ttf-hack otf-font-awesome font-mathematica noto-fonts-cjk noto-fonts-emoji gvim chromium dhclient openssh numlockx gedit xfce4-terminal thunar thunar-volman rofi compton feh xorg-server xorg-xinit xorg-xrandr xorg-xinput xorg-xset mesa maim fuseiso zip unzip unrar p7zip file-roller ntfs-3g poppler-data xss-lock brightnessctl ppp transmission-qt youtube-dl pulseaudio mpv gimp valgrind python python-pip qt qtcreator gdb cmake texlive-most texlive-langchinese biber zotero visual-studio-code-bin
 
