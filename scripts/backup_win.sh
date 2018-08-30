ebee6c75e277addcdb3f2a0f6cf02f5d1b12989a
Fang He <hefun@qq.com>
2018-08-30 19:38:34 +0800

    fix backup_win.sh

--- scripts/backup_win.sh
+++ scripts/backup_win.sh
@@ -3,5 +3,4 @@
 rsync -ahv --delete --ignore-errors --delete-excluded --exclude={"\$RECYCLE.BIN","System Volume Information"} /run/media/enigma/Data /media/stash/backup/win_Data/
 rsync -ahv --delete --ignore-errors --delete-excluded /run/media/enigma/Windows/Users/hfeni/Downloads /media/stash/backup/win_Data/
 rsync -ahv --delete --ignore-errors --delete-excluded /run/media/enigma/Windows/Users/hfeni/Pictures /media/stash/backup/win_Data/
-rsync -ahv --delete --ignore-errors --delete-excluded /run/media/enigma/Windows/Users/hfeni/Documents/WeChat\ Files /media/stash/backup/win_Data/
 
