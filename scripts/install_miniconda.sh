5e0d67506b950944c3c64ceb9950577d914dea24
Fang He <hefun@qq.com>
2018-09-09 16:38:03 +0800

    remove miniconda file after install

--- scripts/install_miniconda.sh
+++ scripts/install_miniconda.sh
@@ -2,6 +2,7 @@
 
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh #(install to ~/.local/miniconda3)
+rm Miniconda3-latest-Linux-x86_64.sh
 
 # echo "alias miniconda='export PATH=~/.local/miniconda3/bin:$PATH'" >> ~/.bashrc
 # open a new terminal, run miniconda
