d1677ce7d08d54b46e8941ce289d7f784050f6cb
Fang He <hefun@qq.com>
2018-09-18 09:52:20 +0800

    update backup scripts

--- scripts/backup_hd.sh
+++ scripts/backup_hd.sh
@@ -2,7 +2,7 @@
 
 # no space after comma
 # no slash after source directory
-rsync -ahv --delete --ignore-errors --delete-excluded --exclude={"MATLAB",".cache","projects/*/Data","projects/AUR/*/*/*","projects/AUR/*/*.tar.?z",".local/share/Trash",".nv",".thumbnails",".gvfs",".config/Code/*Cache*",".config/chromium",".config/Code/logs",".local/share/gvfs-metadata",".local/share/recently-used.xbel*","arch-enigma-home-*.tar.gz",".vscode/extensions"} ~ /media/stash/backup/
+rsync -ahv --delete --ignore-errors --delete-excluded --exclude={"MATLAB",".cache","projects/*/Data","projects/AUR/*/*/*","projects/AUR/*/*.tar.?z",".local/share/Trash",".nv",".thumbnails",".gvfs",".config/Code - OSS/*Cache*",".config/chromium",".config/Code - OSS/logs",".local/share/gvfs-metadata",".local/share/recently-used.xbel*","arch-enigma-home-*.tar.gz",".vscode-oss/extensions"} ~ /media/stash/backup/
 #rsync -ahv --delete --ignore-errors --delete-excluded /etc /media/stash/backup/
 #rsync -ahv --delete --ignore-errors --delete-excluded /boot/loader /media/stash/backup/
 #rsync -ahv --delete --ignore-errors --delete-excluded /usr/lib/modprobe.d /media/stash/backup/
--- scripts/backup_zip.sh
+++ scripts/backup_zip.sh
@@ -4,12 +4,15 @@ tar -I pigz \
     --exclude='.config/mpv/watch_later' \
     --exclude='.config/Atom' \
     --exclude='.config/chromium' \
-    --exclude='.config/Code' \
+    --exclude='.config/Code - OSS' \
+    --exclude='.config/QtProject/qtcreator/qbs' \
+    --exclude='.config/QtProject/qtcreator/.helpcollection' \
     --exclude='projects/**/build' \
     --exclude='projects/**/Data' \
     --exclude='projects/**/Results' \
     --exclude="projects/AUR/*/*.tar.?z" \
     --exclude='projects/AUR/*/*/*' \
+    --exclude='projects/Courses/**/*.pdf' \
     --exclude='.~lock.*' \
     -cvf arch-enigma-home-$(date -I).tar.gz \
     ~/.config \
