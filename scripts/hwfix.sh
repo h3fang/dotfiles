5fd10e55f5d8f4c3a44d48144c0ae06b9520ccd8
Fang He <hefun@qq.com>
2018-12-25 11:45:22 +0800

    switch to nvidia

--- scripts/hwfix.sh
+++ scripts/hwfix.sh
@@ -6,7 +6,7 @@ hda-verb /dev/snd/hwC0D0 0x20 SET_PROC_COEF 0x3000
 mount --bind /mnt/cache/.cache /home/enigma/.cache
 mount --bind /mnt/cache/.config /home/enigma/.config
 
-modprobe acpi_call
-echo "\_SB.PCI0.PEG0.PEGP._OFF" > /proc/acpi/call
-rmmod acpi_call
+#modprobe acpi_call
+#echo "\_SB.PCI0.PEG0.PEGP._OFF" > /proc/acpi/call
+#rmmod acpi_call
 
