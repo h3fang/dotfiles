b53aa69457d77811c138d13970aa47e2253c8ced
Fang He <hefun@qq.com>
2019-02-25 13:30:11 +0800

    fix backup script path

--- scripts/backup_hd.sh
+++ scripts/backup_hd.sh
@@ -2,9 +2,9 @@
 
 # no space after comma
 # no slash after source directory
-rsync -ahv --delete --ignore-errors --delete-excluded --exclude={"MATLAB",".cache","projects/*/Data","projects/*/Results","projects/AUR/*/*/*","projects/AUR/*/*.tar.?z"i,".local/miniconda3",".local/share/Trash",".local/share/Steam",".nv",".thumbnails",".gvfs",".config/Code/*Cache*",".config/Code - OSS/*Cache*",".config/chromium",".config/Code/logs",".config/Code - OSS/logs",".local/share/gvfs-metadata",".local/share/recently-used.xbel*","arch-enigma-home-*.tar.gz",".vscode/extensions",".vscode-oss/extensions"} ~ /media/stash/backup/
+rsync -ahv --delete --ignore-errors --delete-excluded --exclude={"MATLAB",".cache","projects/*/Data","projects/*/Results","projects/AUR/*/*/*","projects/AUR/*/*.tar.?z"i,".local/miniconda3",".local/share/Trash",".local/share/Steam",".nv",".thumbnails",".gvfs",".config/Code/*Cache*",".config/Code - OSS/*Cache*",".config/chromium",".config/Code/logs",".config/Code - OSS/logs",".local/share/gvfs-metadata",".local/share/recently-used.xbel*","arch-enigma-home-*.tar.gz",".vscode/extensions",".vscode-oss/extensions"} ~ /run/media/enigma/stash/backup/
 
 for target in /etc /boot/loader /usr/lib/modprobe.d /usr/lib/modules-load.d ; do
-    rsync -ahv --delete --ignore-errors --delete-excluded $target /media/stash/backup/
+    rsync -ahv --delete --ignore-errors --delete-excluded $target /run/media/enigma/stash/backup/
 done
 
