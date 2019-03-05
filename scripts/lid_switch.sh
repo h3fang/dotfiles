2fe15d6c93700f516a75beb9eba21c619c9a5825
Fang He <hefun@qq.com>
2019-03-05 13:51:28 +0800

    fix last commit

--- scripts/lid_switch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-nohup systemd-inhibit --what=handle-lid-switch --who=$(id -un $(whoami)) --why="Prevent suspend on lid close" --mode=block /usr/bin/bash -c "while true; do sleep 60; done" > /dev/null 2>&1 &
-
