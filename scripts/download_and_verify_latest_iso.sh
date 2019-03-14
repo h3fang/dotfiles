cc3f0c245f55b7e35bc0ab464538a6da6586e356
Fang He <hf.enigma@gmail.com>
2019-03-14 14:29:37 +0800

    update script

--- scripts/download_and_verify_latest_iso.sh
+++ scripts/download_and_verify_latest_iso.sh
@@ -7,5 +7,6 @@ VER=$(date +%Y.%m.01)
 curl -O https://mirrors.ustc.edu.cn/archlinux/iso/$VER/archlinux-$VER-x86_64.iso
 curl -O https://mirrors.ustc.edu.cn/archlinux/iso/$VER/archlinux-$VER-x86_64.iso.sig
 
-gpg --keyserver pgp.mit.edu --keyserver-options auto-key-retrieve --verify archlinux-$VER-x86_64.iso.sig
+#gpg --keyserver pgp.mit.edu --keyserver-options auto-key-retrieve --verify archlinux-$VER-x86_64.iso.sig
+pacman-key -v archlinux-$VER-x86_64.iso.sig
 
