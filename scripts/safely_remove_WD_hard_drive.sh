3607d2fad7c883fd58cb89721e303d3c8abe9401
Fang He <hf.enigma@gmail.com>
2019-03-20 20:09:41 +0800

    sync before unmount

--- scripts/safely_remove_WD_hard_drive.sh
+++ scripts/safely_remove_WD_hard_drive.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sync
 EHD_DRIVE=$(udisksctl status | grep Seagate | grep NA9LNX7B | awk '{print $NF}')
 lsblk -l /dev/$EHD_DRIVE | grep part | awk '{print $1}'| xargs -i udisksctl unmount -b /dev/{}
 udisksctl power-off -b /dev/$EHD_DRIVE
