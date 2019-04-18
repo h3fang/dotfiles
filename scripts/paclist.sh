1afbfb669d027ebb0195be0bd78888895391401c
Fang He <hf.enigma@gmail.com>
2019-04-18 11:53:20 +0800

    refine paclist.sh

--- scripts/paclist.sh
+++ scripts/paclist.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+LOG_FILE=/tmp/pacman_install_log_$(date +%s)
+cat /var/log/pacman.log | sed '/\[ALPM\] installed /s/\[ALPM\] installed //;t;d' > $LOG_FILE
+
 for i in $(pacman -Qqe)
 do
-  grep "\[ALPM\] installed $i (" /var/log/pacman.log | tail -n 1
-done | \
-  sort -u | \
-  sed -e 's/\[ALPM\] installed //' #-e 's/(.*$//'
+    grep "] $i (" $LOG_FILE | tail -n 1
+done | sort
+
+rm $LOG_FILE
 
