1d28b5de44a0c636a279d3e10328bfa4ba2edee9
Fang He <hf.enigma@gmail.com>
2019-05-02 14:44:50 +0800

    update miniconda installation script

--- scripts/install_miniconda.sh
+++ scripts/install_miniconda.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 # silent install to ~/.local/miniconda3
-curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b -p ~/.local/miniconda3
-rm Miniconda3-latest-Linux-x86_64.sh
+MINICONDA_FILE=/tmp/$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 50)
+rm -rf ~/.local/miniconda3
+curl -o $MINICONDA_FILE https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash $MINICONDA_FILE -b -p ~/.local/miniconda3
+rm $MINICONDA_FILE
 
 # prevent conda activate from changing PS1
 echo "changeps1: False" >> ~/.local/miniconda3/.condarc
@@ -14,4 +16,4 @@ conda update conda
 conda create -n main numpy scipy pandas matplotlib scikit-learn pillow h5py xlrd cython
 conda activate main
 conda install tensorflow keras shapely
-pip install --upgrade tensorflow-probability pymc3 vispy
+pip install --upgrade pymc3 vispy #tensorflow-probability
