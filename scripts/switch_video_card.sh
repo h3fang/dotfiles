db12955f5a64a6ab5929dadc3abb22d8334f5ca5
Fang He <hf.enigma@gmail.com>
2019-06-09 10:15:38 +0800

    update and fix nvidia shit

--- scripts/switch_video_card.sh
+++ scripts/switch_video_card.sh
@@ -11,10 +11,7 @@ function use_nvidia
     fi
 
     # do not poweroff nvidia GPU
-    HWFIXPath="/home/enigma/scripts/hwfix.sh"
-    sudo sed -i '/^modprobe acpi_call/ s/^/#/' $HWFIXPath
-    sudo sed -i '/^echo "\\_SB.PCI0.PEG0.PEGP._OFF" > \/proc\/acpi\/call/ s/^/#/' $HWFIXPath
-    sudo sed -i '/^rmmod acpi_call/ s/^/#/' $HWFIXPath
+    sudo systemctl disable disable_nvidia.service
 
     # remove blacklist nvidia card conf
     sudo mv /usr/lib/modprobe.d/graphicscard.conf /usr/lib/modprobe.d/graphicscard.conf.backup
@@ -23,7 +20,7 @@ function use_nvidia
     sed -i '/^xrandr --set/ s/^/#/' ~/.xinitrc
     # enable reverse PRIME for nvidia
     sed -i '/^#xrandr --setprovideroutputsource modesetting NVIDIA-0/ s/^#//' ~/.xinitrc
-    
+
     # setup display
     # uncomment nvidia setting
     sed -i '/^#xrandr --output eDP-1/ s/^#//' ~/.xinitrc
@@ -32,6 +29,13 @@ function use_nvidia
     # comment out nouveau setting
     sed -i '/^xrandr --output eDP1/ s/^/#/' ~/.xinitrc
     sed -i '/^xrandr --output HDMI/ s/^/#/' ~/.xinitrc
+
+    # fix kernel modules
+    sudo sed -i '/^MODULES=(intel_agp i915 nouveau)$/ s/^/#/' /etc/mkinitcpio.conf
+    sudo sed -i '/^#MODULES=(intel_agp i915 nvidia nvidia_modeset nvidia_uvm nvidia_drm)$/ s/^#//' /etc/mkinitcpio.conf
+    sudo sed -i '/^MODULES=(intel_agp i915)$/ s/^/#/' /etc/mkinitcpio.conf
+    sudo mkinitcpio -P
+    sudo mv /etc/pacman.d/hooks/nvidia.hook.backup /etc/pacman.d/hooks/nvidia.hook
 }
 
 function use_intel
@@ -45,10 +49,7 @@ function use_intel
     fi
 
     # poweroff nvidia GPU
-    HWFIXPath="/home/enigma/scripts/hwfix.sh"
-    sudo sed -i '/^#modprobe acpi_call/ s/^#//' $HWFIXPath
-    sudo sed -i '/^#echo "\\_SB.PCI0.PEG0.PEGP._OFF" > \/proc\/acpi\/call/ s/^#//' $HWFIXPath
-    sudo sed -i '/^#rmmod acpi_call/ s/^#//' $HWFIXPath
+    sudo systemctl enable disable_nvidia.service
 
     # blacklist nvidia
     sudo mv /usr/lib/modprobe.d/graphicscard.conf.backup /usr/lib/modprobe.d/graphicscard.conf
@@ -59,6 +60,13 @@ function use_intel
     # setup display
     sed -i '/^xrandr --output / s/^/#/' ~/.xinitrc
     sed -i '/^#xrandr --auto --dpi 144/ s/^#//' ~/.xinitrc
+
+    # fix kernel modules
+    sudo sed -i '/^MODULES=(intel_agp i915 nouveau)$/ s/^/#/' /etc/mkinitcpio.conf
+    sudo sed -i '/^MODULES=(intel_agp i915 nvidia nvidia_modeset nvidia_uvm nvidia_drm)$/ s/^/#/' /etc/mkinitcpio.conf
+    sudo sed -i '/^#MODULES=(intel_agp i915)$/ s/^#//' /etc/mkinitcpio.conf
+    sudo mkinitcpio -P
+    sudo mv /etc/pacman.d/hooks/nvidia.hook /etc/pacman.d/hooks/nvidia.hook.backup
 }
 
 # to get PRIME work with nouveau, xf86-video-nouveau must be installed
@@ -73,10 +81,7 @@ function use_nouveau
     fi
 
     # do not poweroff nvidia GPU
-    HWFIXPath="/home/enigma/scripts/hwfix.sh"
-    sudo sed -i '/^modprobe acpi_call/ s/^/#/' $HWFIXPath
-    sudo sed -i '/^echo "\\_SB.PCI0.PEG0.PEGP._OFF" > \/proc\/acpi\/call/ s/^/#/' $HWFIXPath
-    sudo sed -i '/^rmmod acpi_call/ s/^/#/' $HWFIXPath
+    sudo systemctl disable disable_nvidia.service
 
     # blacklist nvidia
     sudo mv /usr/lib/modprobe.d/graphicscard.conf.backup /usr/lib/modprobe.d/graphicscard.conf
@@ -87,7 +92,7 @@ function use_nouveau
     sed -i '/^xrandr --set/ s/^/#/' ~/.xinitrc
     # enable reverse PRIME for nouveau
     sed -i '/^#xrandr --setprovideroutputsource modesetting nouveau/ s/^#//' ~/.xinitrc
-    
+
     # setup display
     # comment out nvidia setting
     sed -i '/^xrandr --output eDP-1/ s/^/#/' ~/.xinitrc
@@ -96,5 +101,12 @@ function use_nouveau
     # uncomment nouveau setting
     sed -i '/^#xrandr --output eDP1/ s/^#//' ~/.xinitrc
     sed -i '/^#xrandr --output HDMI/ s/^#//' ~/.xinitrc
+
+    # fix kernel modules
+    sudo sed -i '/^#MODULES=(intel_agp i915 nouveau)$/ s/^#//' /etc/mkinitcpio.conf
+    sudo sed -i '/^MODULES=(intel_agp i915 nvidia nvidia_modeset nvidia_uvm nvidia_drm)$/ s/^/#/' /etc/mkinitcpio.conf
+    sudo sed -i '/^MODULES=(intel_agp i915)$/ s/^/#/' /etc/mkinitcpio.conf
+    sudo mkinitcpio -P
+    sudo mv /etc/pacman.d/hooks/nvidia.hook /etc/pacman.d/hooks/nvidia.hook.backup
 }
 
