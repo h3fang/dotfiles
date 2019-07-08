5fa79b9476583c17ea13cd9837c70319ccb0b539
Fang He <hf.enigma@gmail.com>
2019-07-08 12:21:38 +0800

    fix i3 close_apps.sh

--- .config/i3/scripts/close_apps.sh
+++ .config/i3/scripts/close_apps.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
+# requires xorg-xprop, libnotify, udiskie
 
-wmctrl -c Transmission
+# check transmission
+if [[ $(pgrep transmission) ]]; then
+    notify-send -u critical "transmission is still running."
+    exit 1
+fi
 
 i3-msg [class="."] kill
 
 sleep 3
 
-# check transmission
-if [[ $(pgrep transmission) ]]; then
-    notify-send "transmission is still running."
-    exit 1
-fi
-
 # check every window
-all_closed=1
-wmctrl -l | awk '{print $1}' | while read APP; do
-    notify-send "[$APP] is not closed, please check unsaved content and close it manually."
-    $all_closed=0
-done
-
-if [[ $all_closed -eq 0 ]]; then
+n_apps=$(xprop -root | grep '_NET_CLIENT_LIST(WINDOW)' | egrep -o '0x[0-9a-zA-Z]*' | wc -l)
+if [[ $n_apps -gt 0 ]]; then
+    notify-send -u critical "$n_apps applications are still running, please check unsaved content and close it manually."
     exit 2
 fi
 
