ffc39fad023d5ad5778039b972d7ff40c2c85bca
Fang He <hf.enigma@gmail.com>
2019-07-30 09:40:48 +0800

    simplify scripts

--- scripts/printer.sh
+++ scripts/printer.sh
@@ -2,8 +2,7 @@
 
 # hplip is for HP printers, see https://wiki.archlinux.org/index.php/CUPS/Printer-specific_problems
 sudo pacman -S cups hplip
-sudo systemctl enable org.cups.cupsd.service
-sudo systemctl start org.cups.cupsd.service
+sudo systemctl enable --now org.cups.cupsd.service
 
 # open http://localhost:631/ in web browser
 # 1) add current user to groups with printer administration privileges defined in SystemGroup in the /etc/cups/cups-files.conf or 2) login as root
