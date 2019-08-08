5681bc17fa2bc64167b86dfdeea8a5e2b928a5c8
Fang He <hf.enigma@gmail.com>
2019-08-08 10:28:48 +0800

    inhibit idle while chromium is focused and playing

--- .config/sway/scripts/lock.sh
+++ .config/sway/scripts/lock.sh
@@ -1,4 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+
+if [[ $1 != "force" ]]; then
+    chromium_focused=$(swaymsg -t get_tree | jq -r '.. | select(.window_properties?.class == "Chromium") | .focused' | grep "true" | wc -l)
+    chromium_playing=$(pacmd list-sink-inputs | grep 'application.name = "Chromium"' | wc -l)
+    if [[ $chromium_focused -gt 0 && $chromium_playing -gt 0 ]]; then
+        echo "Inhibit idle while chromium is focused and playing"
+        exit 1
+    fi
+fi
 
 IMG=/tmp/swaylock-blur.png
 grim $IMG
