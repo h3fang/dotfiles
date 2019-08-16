a0f9b6547e6234e3df21b7e24b754dfd6a269ec0
Fang He <hf.enigma@gmail.com>
2019-08-16 09:48:34 +0800

    don't update pyglet to latest version

--- scripts/update_miniconda.sh
+++ scripts/update_miniconda.sh
@@ -6,7 +6,9 @@ for env in $(conda info -e | grep miniconda3 | awk '{print $1}'); do
   conda activate $env
   conda update --all
   for pkg in $(conda list | grep pypi$ | awk '{print $1}'); do
+    if [[ $pkg != "pyglet" ]]; then
       pip install --upgrade $pkg
+    fi
   done
   conda clean -a
 done
