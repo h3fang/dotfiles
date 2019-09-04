9678b7491d659246471216c2bd6907b6139137f1
Fang He <hf.enigma@gmail.com>
2019-09-04 19:40:51 +0800

    update i3lock-color date string format

--- .config/i3/scripts/lock.sh
+++ .config/i3/scripts/lock.sh
@@ -9,6 +9,6 @@ revert() {
 trap revert HUP INT TERM
 killall -SIGUSR1 dunst
 
-i3lock -n -t -e --blur=10 --indicator --clock --pass-media-keys --datecolor=ffffffff --timecolor=ffffffff --ringcolor=2075c7ff --time-font=monospace
+i3lock -n -t -e --blur=10 --indicator --clock --pass-media-keys --datestr="%a  %F" --datecolor=ffffffff --timecolor=ffffffff --ringcolor=2075c7ff --time-font=monospace
 
 revert
