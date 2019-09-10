ed3ef6ec6903c7594cade39c5aa2362ec23e7071
Fang He <hf.enigma@gmail.com>
2019-09-10 17:01:33 +0800

    use SJTU mirror instead of USTC one

--- scripts/download_and_verify_latest_iso.sh
+++ scripts/download_and_verify_latest_iso.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-cd ~/Downloads
+cd /tmp
 
 VER=$(date +%Y.%m.01)
 
-curl -O https://mirrors.ustc.edu.cn/archlinux/iso/$VER/archlinux-$VER-x86_64.iso
-curl -O https://mirrors.ustc.edu.cn/archlinux/iso/$VER/archlinux-$VER-x86_64.iso.sig
+curl -O https://mirrors.sjtug.sjtu.edu.cn/archlinux/iso/$VER/archlinux-$VER-x86_64.iso
+curl -O https://mirrors.sjtug.sjtu.edu.cn/archlinux/iso/$VER/archlinux-$VER-x86_64.iso.sig
 
 #gpg --keyserver pgp.mit.edu --keyserver-options auto-key-retrieve --verify archlinux-$VER-x86_64.iso.sig
 pacman-key -v archlinux-$VER-x86_64.iso.sig
-
