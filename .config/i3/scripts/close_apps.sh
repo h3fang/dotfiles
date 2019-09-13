ec3e435f2d3670756289e05b04fcf11630e4692e
Fang He <hf.enigma@gmail.com>
2019-09-13 10:52:30 +0800

    fix visible windows couting

--- .config/i3/scripts/close_apps.sh
+++ .config/i3/scripts/close_apps.sh
@@ -12,7 +12,7 @@ i3-msg [class="."] kill
 sleep 3
 
 # check every window
-n_apps=$(xprop -root | grep '_NET_CLIENT_LIST(WINDOW)' | egrep -o '0x[0-9a-zA-Z]*' | wc -l)
+n_apps=$(xprop -root | grep '^_NET_CLIENT_LIST(WINDOW)' | egrep -o '0x[0-9a-zA-Z]+' | wc -l)
 if [[ $n_apps -gt 0 ]]; then
     notify-send -u critical "$n_apps applications are still running, please check unsaved content and close it manually."
     exit 2
