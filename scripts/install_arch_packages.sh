f89a2c0fd97ad623354c68ba5288d086a841d009
Fang He <hf.enigma@gmail.com>
2019-09-15 22:44:27 +0800

    add post installation configuration

--- scripts/install_arch_packages.sh
+++ scripts/install_arch_packages.sh
@@ -12,19 +12,19 @@ mv dotfiles/* ~
 cd /tmp
 git clone https://aur.archlinux.org/yay.git
 cd yay
-PKGEXT=.pkg.tar makepkg -sri
+PKGEXT=.pkg.tar makepkg -fsri
 
 # fonts
-yay -S ttf-dejavu ttf-liberation ttf-hack otf-font-awesome noto-fonts-cjk noto-fonts-emoji
+yay -S ttf-dejavu ttf-liberation ttf-hack ttf-roboto otf-font-awesome noto-fonts noto-fonts-cjk noto-fonts-emoji
 
 # drivers
 yay -S mesa pulseaudio
-z
+
 # system tools
-yay -S htop gvim gedit fuseiso zip unzip unrar p7zip file-roller ntfs-3g openssh poppler-data mpv gimp
+yay -S htop gvim fd ncdu gedit fuseiso zip unzip unrar p7zip file-roller ntfs-3g openssh poppler-data mpv gimp
 
 # network
-yay -S networkmanager dhclient ppp transmission-qt youtube-dl chromium
+yay -S networkmanager network-manager-applet dhclient ppp sstp-client transmission-qt youtube-dl chromium
 
 # xorg
 yay -S xorg-server xorg-xinit xorg-xrandr xorg-xinput xorg-xset xorg-xprop
@@ -36,7 +36,30 @@ yay -S texlive-bibtexextra texlive-core texlive-formatsextra texlive-humanities 
 yay -S python clang gdb cmake visual-studio-code-bin
 
 # i3
-yay -S i3-wm i3blocks i3lock numlockx xfce4-terminal thunar thunar-volman gvfs rofi compton feh xss-lock brightnessctl
+yay -S i3-wm i3blocks i3lock-color numlockx xfce4-terminal thunar thunar-volman gvfs rofi compton feh xss-lock brightnessctl
 
 # academic
 yay -S zotero
+
+### self-maintained AUR packages
+cd ~/projects
+git clone https://github.com/h3fang/AUR.git
+cd AUR
+for package_dir in $(fd -t d -d 1); do
+    cd $package_dir
+    PKGEXT=.pkg.tar makepkg -fsri
+    cd ..
+done
+
+### post-install configuration
+sudo systemctl enable now NetworkManager.service doh-client.service earlyoom.service
+
+# adjust the log level for NetwokManager dhcp component, wpa_supplicant, doh-client
+echo -e '[logging]\nlevel=WARN\ndomains=DHCP' | sudo tee /etc/NetworkManager/conf.d/dhcp-logging.conf > /dev/null
+echo -e '[Service]\nLogLevelMax=4' | sudo SYSTEMD_EDITOR=tee systemctl edit wpa_supplicant.service doh-client.service
+
+# earlyoom
+sudo sed -i 's|^EARLYOOM_ARGS=.*|EARLYOOM_ARGS="-m 5 -r 0 -N '/usr/bin/notify-all'"|' /etc/default/earlyoom
+
+# user backup service
+systemctl enable --now --user backup_borg.timer
