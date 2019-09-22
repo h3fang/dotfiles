dfd98ce55f3457b3bd8fb19380480ec826910a7c
Fang He <hf.enigma@gmail.com>
2019-09-22 19:28:26 +0800

    diable at-spi2

--- scripts/post_chroot.sh
+++ scripts/post_chroot.sh
@@ -61,6 +61,10 @@ echo -e 'blacklist btusb\nblacklist bluetooth' | sudo tee /etc/modprobe.d/blueto
 echo 'blacklist uvcvideo' | sudo tee /etc/modprobe.d/camera.conf
 echo 'blacklist pcspkr' | sudo tee /etc/modprobe.d/nobeep.conf
 
+# at-spi2
+echo 'NO_AT_BRIDGE=1' | sudo tee -a /etc/environment
+echo 'NoExtract = usr/share/dbus-1/services/org.a11y.*' | sudo tee -a /etc/pacman.conf
+
 # adjust the log level for NetwokManager dhcp component, wpa_supplicant, doh-client
 echo -e '[logging]\nlevel=WARN\ndomains=DHCP' | sudo tee /etc/NetworkManager/conf.d/dhcp-logging.conf > /dev/null
 echo -e '[Service]\nLogLevelMax=4' | sudo SYSTEMD_EDITOR=tee systemctl edit wpa_supplicant.service doh-client.service
