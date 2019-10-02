ad9320711f3e39932acf3931eb10b02be854188b
Fang He <hf.enigma@gmail.com>
2019-10-02 13:55:45 +0800

    add alternative; remove unnecessary settings

--- scripts/install_wine.sh
+++ scripts/install_wine.sh
@@ -5,7 +5,7 @@ trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
 sudo pacman -S --needed wine-staging wine_gecko wine-mono lib32-nvidia-utils lib32-libpulse lib32-libxml2 lib32-mpg123 lib32-lcms2 lib32-giflib lib32-libpng lib32-gnutls lib32-libldap lib32-libgpg-error lib32-gst-plugins-base lib32-gst-plugins-good winetricks
 
-PKGEXT=.pkg.tar yay -S --needed dxvk-bin
+PKGEXT=.pkg.tar yay -S --needed dxvk-bin #dxvk-winelib-git
 
 # use wine prefix in ~/.cache/wine instead of default ~/.wine, this is also exported in ~/scripts/envs
 export WINEPREFIX=~/.cache/wine
@@ -15,4 +15,4 @@ wait $(pgrep wineboot)
 # dxvk
 setup_dxvk install
 # misc
-winetricks vcrun2015 corefonts fakechinese fontsmooth=rgb mimeassoc=off win10 isolate_home
+winetricks vcrun2015 fontsmooth=rgb mimeassoc=off win10 isolate_home
