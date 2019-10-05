e623749602b4765a2a6c1876756c8e886902b7de
Fang He <hf.enigma@gmail.com>
2019-10-05 19:21:54 +0800

    use udiskie if possible

--- scripts/unmount_all_removable_media.sh
+++ scripts/unmount_all_removable_media.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
+# use udiskie-umount if possible
 # no extra dependency
 
-sync
+if [[ -x $(command -v udiskie-umount) ]]; then
+    udiskie-umount -ad
+else
+    sync
 
-for device in /sys/block/*
-do
-    if udevadm info --query=property --path=$device | grep -q ^ID_BUS=usb; then
-        echo found $device to unmount
-        DEVTO=`echo $device|awk -F"/" 'NF>1{print $NF}'`
-        echo `df -h|grep "$(ls /dev/$DEVTO*)"|awk '{print $1}'` is the exact device
-        UM=`df -h|grep "$(ls /dev/$DEVTO*)"|awk '{print $1}'`
-        if umount $UM; then
-            echo umounted $UM
-        else
-            echo failed to unmount $UM
+    for device in /sys/block/*
+    do
+        if udevadm info --query=property --path=$device | grep -q ^ID_BUS=usb; then
+            echo found $device to unmount
+            DEVTO=`echo $device|awk -F"/" 'NF>1{print $NF}'`
+            echo `df -h|grep "$(ls /dev/$DEVTO*)"|awk '{print $1}'` is the exact device
+            UM=`df -h|grep "$(ls /dev/$DEVTO*)"|awk '{print $1}'`
+            if umount $UM; then
+                echo umounted $UM
+            else
+                echo failed to unmount $UM
+            fi
         fi
-    fi
-done
-
+    done
+fi
