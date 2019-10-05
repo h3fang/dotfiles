552e0e337e7795630713fec72ab73d80ff3b29ca
Fang He <hf.enigma@gmail.com>
2019-10-05 14:02:52 +0800

    much fast processing & no disk write for paclist script

---
+++ scripts/paclist.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import subprocess
+
+explicit_pkgs = subprocess.check_output("pacman -Qqe", shell=True, text=True)
+installed = []
+signature = "[ALPM] installed "
+with open("/var/log/pacman.log", "r") as f:
+    installed = [ln.replace(signature, "").replace("\n", "") for ln in f if signature in ln]
+
+output = []
+for i in explicit_pkgs.split():
+    for ln in installed:
+        if i in ln:
+            output.append(ln)
+            break
+
+for ln in sorted(output):
+    print(ln)
