9bd90cfd3e986d143c75516a8704b2a34e7178c1
Fang He <hf.enigma@gmail.com>
2019-10-06 18:08:53 +0800

    allow unloading nvidia modules to fail

--- scripts/disable_nvidia_card.sh
+++ scripts/disable_nvidia_card.sh
@@ -6,7 +6,7 @@ trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 CONTROLLER_BUS_ID=0000:00:01.0
 DEVICE_BUS_ID=0000:01:00.0
 
-rmmod nvidia_drm nvidia_modeset nvidia_uvm nvidia
+rmmod nvidia_drm nvidia_modeset nvidia_uvm nvidia || true
 
 echo 'Removing Nvidia bus from the kernel'
 tee /sys/bus/pci/devices/${DEVICE_BUS_ID}/remove <<<1
