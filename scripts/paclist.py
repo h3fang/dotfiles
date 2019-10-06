5ec86907cccb94fd08e784e5c8cae9120c8d5adf
Fang He <hf.enigma@gmail.com>
2019-10-06 15:00:09 +0800

    use hash table to further improve performance & eliminate potentional mismatch

--- scripts/paclist.py
+++ scripts/paclist.py
@@ -2,20 +2,22 @@
 
 import subprocess
 
-explicit_pkgs = subprocess.check_output("pacman -Qqe", shell=True, text=True)
-installed = []
+explicit_pkgs = subprocess.check_output("pacman -Qqe", shell=True, text=True).split("\n")
+installed = {}
 signature = "[ALPM] installed "
-with open("/var/log/pacman.log", "r") as f:
-    installed = [ln.replace(signature, "").replace("\n", "") for ln in f if signature in ln]
+with open("/var/log/pacman.log", "rt") as f:
+    for ln in reversed(list(f)):
+        if signature in ln:
+            tokens = ln.replace(signature, "").replace("\n", "").split(" ")
+            if tokens[2] in installed:
+                continue
+            else:
+                installed[tokens[2]] = tokens
 
-installed = installed[::-1]
-explicit_pkgs = [' '+e+' ' for e in explicit_pkgs.split()]
 output = []
-for i in explicit_pkgs:
-    for ln in installed:
-        if i in ln:
-            output.append(ln)
-            break
+for e in explicit_pkgs:
+    if e:
+        output.append(" ".join(installed[e]))
 
 for ln in sorted(output):
     print(ln)
