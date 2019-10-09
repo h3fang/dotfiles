176b93562b0a8cf9c1dafd8797dfc4a3cac0ff6a
Fang He <hf.enigma@gmail.com>
2019-10-09 10:58:49 -0600

    suppress stdout

--- scripts/fix_sound_card.sh
+++ scripts/fix_sound_card.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # need alsa-tools
-hda-verb /dev/snd/hwC0D0 0x20 SET_COEF_INDEX 0x67
-hda-verb /dev/snd/hwC0D0 0x20 SET_PROC_COEF 0x3000
+hda-verb /dev/snd/hwC0D0 0x20 SET_COEF_INDEX 0x67 > /dev/null
+hda-verb /dev/snd/hwC0D0 0x20 SET_PROC_COEF 0x3000 > /dev/null
 
