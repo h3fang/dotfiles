cdbabf28bc27123557bb5e1c7a8792db932ba347
Fang He <hf.enigma@gmail.com>
2019-10-19 18:38:24 +0800

    download to specified location if possible

--- scripts/download_and_verify_latest_iso.sh
+++ scripts/download_and_verify_latest_iso.sh
@@ -1,9 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-mkdir -p ~/Downloads
-cd ~/Downloads
+DDIR=~/Downloads
+if [[ -n $1 ]]; then
+    DDIR="$1"
+fi
+mkdir -p $DDIR
+cd $DDIR
 
 VER=$(date +%Y.%m.01)
+
+echo "Downloading latest arch iso to $DDIR ..."
 
 curl -O https://mirrors.sjtug.sjtu.edu.cn/archlinux/iso/$VER/archlinux-$VER-x86_64.iso
 curl -O https://mirrors.sjtug.sjtu.edu.cn/archlinux/iso/$VER/archlinux-$VER-x86_64.iso.sig
