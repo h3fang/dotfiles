2a1d08825d2bad0fe65fcb7fb154df2d76bc9a4e
Fang He <hf.enigma@gmail.com>
2019-11-17 11:04:26 +0800

    use UUID to identify partitions (persistent block device naming)

--- scripts/mount.sh
+++ scripts/mount.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 mkdir -p /run/media/enigma/Apps
-ntfs-3g /dev/sdb1 /run/media/enigma/Apps
+mount -t ntfs-3g -U 4CD6F67DD6F6669C /run/media/enigma/Apps
 
 mkdir -p /run/media/enigma/Data
-ntfs-3g /dev/sdb3 /run/media/enigma/Data
+mount -t ntfs-3g -U F27402DA7402A189 /run/media/enigma/Data
 
 mkdir -p /run/media/enigma/Stash
-ntfs-3g /dev/sdb5 /run/media/enigma/Stash
+mount -t ntfs-3g -U DA8E17968E176A71 /run/media/enigma/Stash
 
