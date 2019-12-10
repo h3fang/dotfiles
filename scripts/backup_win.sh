8ce84852688a8b6633d9537f95674d22f7090ead
Fang He <hf.enigma@gmail.com>
2019-12-10 20:46:19 +0800

    update backup script

--- scripts/backup_win.sh
+++ scripts/backup_win.sh
@@ -4,6 +4,3 @@
 PREFIX=/run/media/enigma
 
 rsync -ahv --delete --ignore-errors --delete-excluded --exclude={"\$RECYCLE.BIN","System Volume Information"} $PREFIX/Data $PREFIX/stash/backup/win_Data/
-rsync -ahv --delete --ignore-errors --delete-excluded $PREFIX/Windows/Users/hfeni/Downloads $PREFIX/stash/backup/win_Data/
-rsync -ahv --delete --ignore-errors --delete-excluded $PREFIX/Windows/Users/hfeni/Pictures $PREFIX/stash/backup/win_Data/
-
