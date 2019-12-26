62b788e6cdab05576ff4aa441309a4782cd8e2e1
Fang He <hf.enigma@gmail.com>
2019-12-26 11:47:09 +0800

    update unmount script

--- scripts/unmount_WD_hard_drive.sh
+++ scripts/unmount_WD_hard_drive.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sync
-EHD_DRIVE=$(udisksctl status | grep Seagate | grep NA9LNX7B | awk '{print $NF}')
+set -eEuo pipefail
+trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+
+EHD_DRIVE=$(udisksctl status | grep 'ST2000LM007-1R8174' | grep 'WDZCVFJD' | awk '{print $NF}')
 lsblk -l /dev/$EHD_DRIVE | grep part | awk '{print $1}'| xargs -i udisksctl unmount -b /dev/{}
 udisksctl power-off -b /dev/$EHD_DRIVE
 echo "powered-off /dev/$EHD_DRIVE"
-
