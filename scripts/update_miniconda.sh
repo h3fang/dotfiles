dc3023571950b3aacd285fbfc413dd860527dfcb
Fang He <hf.enigma@gmail.com>
2020-01-27 12:24:34 +0800

    ask before updating

--- scripts/update_miniconda.sh
+++ scripts/update_miniconda.sh
@@ -9,18 +9,21 @@ GREEN="\e[38;2;0;255;0m"
 CLR="\e[0m"
 
 for env in $(conda info -e | grep miniconda3 | awk '{print $1}'); do
-  echo -e "${GREEN}updating environment ${RED}${env}${GREEN} ...${CLR}"
-  conda activate "$env"
-  # conda packages
-  conda update --all
-  # pip packages
-  for pkg in $(conda list | grep pypi$ | awk '{print $1}'); do
-    # gym requires a specific version of pyglet
-    if [[ $pkg != "pyglet" ]]; then
-      pip install --upgrade "$pkg"
-    fi
-  done
-  conda deactivate
+    echo -en "${GREEN}updating environment ${RED}${env}${GREEN} ? (y/[n])${CLR}"
+  read -r
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    conda activate "$env"
+    # conda packages
+    conda update --all
+    # pip packages
+    for pkg in $(conda list | grep pypi$ | awk '{print $1}'); do
+      # gym requires a specific version of pyglet
+      if [[ $pkg != "pyglet" ]]; then
+        pip install --upgrade "$pkg"
+      fi
+    done
+    conda deactivate
+  fi
 done
 
 for env in $(conda info -e | grep miniconda3 | awk '{print $1}'); do
