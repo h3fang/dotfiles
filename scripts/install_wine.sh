a5ad17e130c70754c94548fd047ff3ea815f5fc2
Fang He <hf.enigma@gmail.com>
2020-06-27 14:21:48 +0800

    update wine dependencies

--- scripts/install_wine.sh
+++ scripts/install_wine.sh
@@ -7,10 +7,12 @@ trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 sudo sed -i 's/^#\[multilib\]$/\[multilib\]/' /etc/pacman.conf
 sudo sed -i '/^\[multilib\]$/{n; s/^#Include = \/etc\/pacman.d\/mirrorlist$/Include = \/etc\/pacman.d\/mirrorlist/}' /etc/pacman.conf
 
+# update first to fetch multilib database
+sudo pacman -Syu
 
 sudo pacman -S --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
 
-PKGEXT=.pkg.tar yay -S --needed dxvk-bin
+PKGEXT=.pkg.tar yay -S --needed --editmenu dxvk-mingw
 
 # use wine prefix in ~/.cache/wine instead of default ~/.wine, this is also exported in ~/scripts/envs
 export WINEPREFIX=~/.cache/wine
