12e69961948e84c7fd01a4bd48784c2cd8d10fad
Fang He <hf.enigma@gmail.com>
2020-07-05 10:14:30 +0800

    update wine scripts

--- scripts/install_wine.sh
+++ scripts/install_wine.sh
@@ -10,14 +10,17 @@ sudo sed -i '/^\[multilib\]$/{n; s/^#Include = \/etc\/pacman.d\/mirrorlist$/Incl
 # update first to fetch multilib database
 sudo pacman -Syu
 
-sudo pacman -S --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
-
-PKGEXT=.pkg.tar yay -S --needed --editmenu dxvk-mingw
+wine="wine-staging"
+dxvk="dxvk-bin" #dxvk-mingw
+graphics_driver="lib32-nvidia-utils" #lib32-mesa
+sudo yay -S --needed $wine $dxvk $graphics_driver lib32-giflib lib32-libpng lib32-libldap lib32-gnutls lib32-mpg123 lib32-openal lib32-v4l-utils lib32-libpulse lib32-libgpg-error lib32-alsa-plugins lib32-alsa-lib lib32-libjpeg-turbo lib32-sqlite lib32-libxcomposite lib32-libgcrypt lib32-libxinerama lib32-ncurses ocl-icd lib32-ocl-icd lib32-libxslt lib32-libva lib32-gtk3 lib32-gst-plugins-base-libs lib32-vulkan-icd-loader
 
 # use wine prefix in ~/.cache/wine instead of default ~/.wine, this is also exported in ~/scripts/envs
 export WINEPREFIX=~/.cache/wine
 # initialize wine prefix
 wineboot -u
-wait $(pgrep wineboot) || true
+tail --pid=$(pgrep wineboot) -f /dev/null
+tail --pid=$(pgrep wineserver) -f /dev/null
+sleep 3
 # dxvk
-setup_dxvk install
+setup_dxvk install --symlink
--- scripts/uninstall_wine.sh
+++ scripts/uninstall_wine.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-yay -Rns dxvk-mingw wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
+set -e
+
+yay -Rns $(pacman -Qq wine dxvk lib32-nvidia-utils) lib32-giflib lib32-libpng lib32-libldap lib32-gnutls lib32-mpg123 lib32-openal lib32-v4l-utils lib32-libpulse lib32-libgpg-error lib32-alsa-plugins lib32-alsa-lib lib32-libjpeg-turbo lib32-sqlite lib32-libxcomposite lib32-libgcrypt lib32-libxinerama lib32-ncurses ocl-icd lib32-ocl-icd lib32-libxslt lib32-libva lib32-gtk3 lib32-gst-plugins-base-libs lib32-vulkan-icd-loader
 
 read -p "Disable multilib? y/[n]" -n 1
 
@@ -11,13 +13,7 @@ fi
 
 # delete wine related
 rm -rf $WINEPREFIX ~/.cache/winetricks
-rm -f ~/.config/menus/applications-merged/wine-*
-rm -rf ~/.local/share/applications/wine
-rm -f ~/.local/share/icons/hicolor/*/*/application-x-wine-extension*
-rm -f ~/.local/share/applications/mimeinfo.cache
-rm -f ~/.local/share/mime/packages/x-wine*
-rm -f ~/.local/share/mime/application/x-wine-extension*
-
-# delete if empty
-rmdir -p ~/.config/menus || true
-rmdir -p ~/.local/share/{applications,icons,mime} || true
+rm -r ~/.config/menus
+rm -r ~/.local/share/applications
+rm -r ~/.local/share/icons
+rm -r ~/.local/share/mime
