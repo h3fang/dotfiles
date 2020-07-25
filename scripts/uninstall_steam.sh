717509fa08c361cc497f0e0651a7c0ff3a65bacc
Fang He <hf.enigma@gmail.com>
2020-07-25 09:30:50 +0800

    fix bash syntax

--- scripts/uninstall_steam.sh
+++ scripts/uninstall_steam.sh
@@ -4,7 +4,7 @@ sudo pacman -Rns steam lib32-nvidia-utils
 
 read -p "Disable multilib? y/[n]" -n 1
 
-if [[ $REPLY == "y"]]; then
+if [[ $REPLY == "y" ]]; then
     sudo sed -i 's/^\[multilib\]$/#\[multilib\]/' /etc/pacman.conf
     sudo sed -i '/^#\[multilib\]$/{n; s/^Include = \/etc\/pacman.d\/mirrorlist$/#Include = \/etc\/pacman.d\/mirrorlist/}' /etc/pacman.conf
 fi
