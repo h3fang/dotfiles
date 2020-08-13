45d60a1ca302de6c7987897a6679dec2c6d5ca61
Fang He <hf.enigma@gmail.com>
2020-08-13 10:56:25 +0800

    wine related changes

--- scripts/install_steam.sh
+++ scripts/install_steam.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -e
+
 # should be fine if it is already enabled
 sudo sed -i 's/^#\[multilib\]$/\[multilib\]/' /etc/pacman.conf
 sudo sed -i '/^\[multilib\]$/{n; s/^#Include = \/etc\/pacman.d\/mirrorlist$/Include = \/etc\/pacman.d\/mirrorlist/}' /etc/pacman.conf
 
-sudo pacman -Syu steam lib32-nvidia-utils
+sudo pacman -Syu steam lib32-mesa
--- scripts/install_wine.sh
+++ scripts/install_wine.sh
@@ -11,8 +11,8 @@ sudo sed -i '/^\[multilib\]$/{n; s/^#Include = \/etc\/pacman.d\/mirrorlist$/Incl
 sudo pacman -Syu
 
 wine="wine-staging"
-dxvk="dxvk-bin" #dxvk-mingw
-graphics_driver="lib32-nvidia-utils" #lib32-mesa
+dxvk="dxvk-bin"
+graphics_driver=lib32-mesa
 sudo yay -S --needed $wine $dxvk $graphics_driver lib32-giflib lib32-libpng lib32-libldap lib32-gnutls lib32-mpg123 lib32-openal lib32-v4l-utils lib32-libpulse lib32-libgpg-error lib32-alsa-plugins lib32-alsa-lib lib32-libjpeg-turbo lib32-sqlite lib32-libxcomposite lib32-libgcrypt lib32-libxinerama lib32-ncurses ocl-icd lib32-ocl-icd lib32-libxslt lib32-libva lib32-gtk3 lib32-gst-plugins-base-libs lib32-vulkan-icd-loader
 
 # use wine prefix in ~/.cache/wine instead of default ~/.wine, this is also exported in ~/scripts/envs
