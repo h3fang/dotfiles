5f0aada0060c970dfe7c5a4f300d4987debb11e2
Fang He <hf.enigma@gmail.com>
2020-08-21 09:06:20 +0800

    remove sudo from yay; check before waiting for process

--- scripts/install_wine.sh
+++ scripts/install_wine.sh
@@ -13,14 +13,18 @@ sudo pacman -Syu
 wine="wine-staging"
 dxvk="dxvk-bin"
 graphics_driver=lib32-mesa
-sudo yay -S --needed $wine $dxvk $graphics_driver lib32-giflib lib32-libpng lib32-libldap lib32-gnutls lib32-mpg123 lib32-openal lib32-v4l-utils lib32-libpulse lib32-libgpg-error lib32-alsa-plugins lib32-alsa-lib lib32-libjpeg-turbo lib32-sqlite lib32-libxcomposite lib32-libgcrypt lib32-libxinerama lib32-ncurses ocl-icd lib32-ocl-icd lib32-libxslt lib32-libva lib32-gtk3 lib32-gst-plugins-base-libs lib32-vulkan-icd-loader
+yay -S --needed $wine $dxvk $graphics_driver lib32-giflib lib32-libpng lib32-libldap lib32-gnutls lib32-mpg123 lib32-openal lib32-v4l-utils lib32-libpulse lib32-libgpg-error lib32-alsa-plugins lib32-alsa-lib lib32-libjpeg-turbo lib32-sqlite lib32-libxcomposite lib32-libgcrypt lib32-libxinerama lib32-ncurses ocl-icd lib32-ocl-icd lib32-libxslt lib32-libva lib32-gtk3 lib32-gst-plugins-base-libs lib32-vulkan-icd-loader
 
 # use wine prefix in ~/.cache/wine instead of default ~/.wine, this is also exported in ~/scripts/envs
 export WINEPREFIX=~/.cache/wine
 # initialize wine prefix
 wineboot -u
-tail --pid=$(pgrep wineboot) -f /dev/null
-tail --pid=$(pgrep wineserver) -f /dev/null
+if pgrep wineboot; then
+    tail --pid=$(pgrep wineboot) -f /dev/null
+fi
+if pgrep wineserver; then
+    tail --pid=$(pgrep wineserver) -f /dev/null
+fi
 sleep 3
 # dxvk
 setup_dxvk install --symlink
