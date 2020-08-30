3311ede4b9e682fac81cf4a8c5902d427ff60147
Fang He <hf.enigma@gmail.com>
2020-08-30 09:01:58 +0800

    simplify the update process for AUR packages

--- scripts/update_AUR_packages.sh
+++ scripts/update_AUR_packages.sh
@@ -1,21 +1,13 @@
 #!/bin/sh
 
 # update system first
-sudo pacman -Syu
-
-# update yay
-# yay -S yay
+yay -Syu
 
 # self-maintained packages
 cd ~/projects/AUR
-for pkg in $(yay -Qgq modified) ; do
-    cd $pkg-h3f
-    PKGEXT=.pkg.tar makepkg -fsricC
+for pkg in $(pacman -Qgq modified) ; do
+    cd $pkg
+    makepkg -fsricC
     cd ..
 done
 
-# AUR source packages
-yay -S brightnessctl hstr-git wlroots-git sway-git swaybg-git swayidle-git swaylock-git
-
-# AUR binary packages
-# yay -S visual-studio-code-bin zotero
