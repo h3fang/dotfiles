2d2768003afbb01f9f08c525e145ed4b173fe663
Fang He <hf.enigma@gmail.com>
2020-09-09 08:53:16 +0800

    kill compositor before lockup

--- .config/i3/scripts/lock.sh
+++ .config/i3/scripts/lock.sh
@@ -4,10 +4,12 @@
 
 revert() {
     killall -SIGUSR2 dunst
+    picom &
 }
 
 trap revert HUP INT TERM
 killall -SIGUSR1 dunst
+pkill -x picom
 
 i3lock -n -t -e --blur=10 --indicator --clock --pass-media-keys --datestr="%a  %F" --datecolor=ffffffff --timecolor=ffffffff --ringcolor=2075c7ff --time-font=monospace
 
