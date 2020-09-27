0f70b51623971e0e1cd3e1fb899187eabd4a5da2
Fang He <hf.enigma@gmail.com>
2020-09-27 09:25:41 +0800

    add alias and alternative commands to cleaning list

--- scripts/clean_bash_history.sh
+++ scripts/clean_bash_history.sh
@@ -11,10 +11,13 @@ sed -Ei '/PYTHONIOENCODING=UTF-8/d' ~/.bash_history
 sed -Ei '/^(sudo )?(pacman|yay|aurman|pikaur|pacaur) /d' ~/.bash_history
 
 # delete cd, rm, man, ls, etc.
-sed -Ei '/^(sudo )?(cd|rm|ls|cat|man|which|[pfe]?grep|cp|mv|fd|pip)( |$)/d' ~/.bash_history
+sed -Ei '/^(sudo )?(cd|rm|ls|ll|tree|cat|man|which|[pfe]?grep|cp|mv|fd|find|pip)( |$)/d' ~/.bash_history
+
+# curl, wget, youtube-dl
+sed -Ei '/^(sudo )?(curl|wget|youtube-dl) /d' ~/.bash_history
 
 # delete vim commands
-sed -Ei '/^(sudo )?vim /d' ~/.bash_history
+sed -Ei '/^(sudo )?(vim|nvim) /d' ~/.bash_history
 
 # list top 10 commands
 awk '{print $1}' .bash_history | sort | uniq -c | sort -n | tail -10
