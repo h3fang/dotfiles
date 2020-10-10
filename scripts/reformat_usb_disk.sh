031832316be74e9a49ca3094ab0baf9f2500f541
Fang He <hf.enigma@gmail.com>
2020-10-10 14:52:27 +0800

    reformat usb as mbr disk

--- scripts/reformat_usb_disk.sh
+++ scripts/reformat_usb_disk.sh
@@ -8,7 +8,7 @@ if [[ $REPLY == "yes" ]]
 then
     udiskie-umount /dev/${usb_disk}1
     sudo wipefs --all /dev/${usb_disk}
-    sudo parted -s /dev/${usb_disk} mklabel gpt mkpart primary fat32 0% 100%
+    sudo parted -s /dev/${usb_disk} mklabel msdos mkpart primary fat32 0% 100%
     udiskie-umount /dev/${usb_disk}1
     sudo mkfs.fat -F 32 -n HEF /dev/${usb_disk}1
 fi
