728bde4a9bfd4e2f5d668604a867fa780b7e2924
Fang He <hf.enigma@gmail.com>
2021-03-10 10:13:11 +0800

    fix the delay before i3lock shows up after resuming from suspend

--- .config/i3/scripts/lock.sh
+++ .config/i3/scripts/lock.sh
@@ -2,15 +2,41 @@
 
 # Dependencies: i3lock-color
 
-revert() {
-    killall -SIGUSR2 dunst
-    picom &
+i3lock_options=(-t -e --blur=20 --indicator --clock --pass-media-keys --datestr="%a  %F" --datecolor=ffffffff --timecolor=ffffffff --ringcolor=2075c7ff --time-font=monospace)
+
+pre_lock() {
+    killall -SIGUSR1 dunst
+    pkill -x picom
 }
 
-trap revert HUP INT TERM
-killall -SIGUSR1 dunst
-pkill -x picom
+post_lock() {
+    killall -SIGUSR2 dunst
+    picom -b
+}
 
-i3lock -n -t -e --blur=20 --indicator --clock --pass-media-keys --datestr="%a  %F" --datecolor=ffffffff --timecolor=ffffffff --ringcolor=2075c7ff --time-font=monospace
+trap post_lock HUP INT TERM
 
-revert
+pre_lock
+
+if [[ -e /dev/fd/${XSS_SLEEP_LOCK_FD:--1} ]]; then
+    kill_i3lock() {
+        pkill -xu $EUID "$@" i3lock
+    }
+
+    trap kill_i3lock TERM INT
+
+    i3lock "${i3lock_options[@]}" {XSS_SLEEP_LOCK_FD}<&-
+
+    exec {XSS_SLEEP_LOCK_FD}<&-
+
+    while kill_i3lock -0; do
+        sleep 0.5
+    done
+else
+    trap 'kill %%' TERM INT
+    i3lock -n "${i3lock_options[@]}" &
+    wait
+fi
+
+post_lock
+
