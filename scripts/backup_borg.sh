cf9d259d51ccad84dbc403f40ec06b01db5badc0
Fang He <hf.enigma@gmail.com>
2021-03-27 21:20:53 +0800

    show less information for rclone when doing backup

--- scripts/backup_borg.sh
+++ scripts/backup_borg.sh
@@ -127,7 +127,7 @@ function f_prune {
 function f_sync {
     for remote in "${RCLONE_REMOTE[@]}"; do
         echo -e "\nuploading to ${remote} ..."
-        rclone --stats-one-line -P --stats 1s --drive-use-trash=false sync "$REPO" "${remote}:${PREFIX}-borg" -v --timeout=30s --fast-list --transfers=10
+        rclone -P --drive-use-trash=false sync "$REPO" "${remote}:${PREFIX}-borg" --timeout=30s --fast-list --transfers=10
     done
 }
 
