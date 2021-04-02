8c76c72b217e7d106f6578f6c3c5162f505c7c3b
Fang He <hf.enigma@gmail.com>
2021-04-02 14:12:36 +0800

    update local backup script

--- scripts/backup_to_local.sh
+++ scripts/backup_to_local.sh
@@ -1,40 +1,29 @@
 #!/bin/sh
 
-BACKUP_LOC=/run/media/enigma/stash/backup/
+BACKUP_LOC=/run/media/$USER/stash/backup/
 
-# no slash after source directory
-rsync -ahv --delete --ignore-errors --delete-excluded \
-    --exclude="MATLAB" \
-    --exclude="arch-${USER}-*.tar.zst" \
-    --exclude="projects/*/[dD]ata" \
-    --exclude="projects/*/[rR]esults/**/*.npz" \
+rsync -ahv --delete --delete-excluded \
     --exclude="projects/AUR/*/*/*" \
     --exclude="projects/AUR/*/*.tar.?z" \
     --exclude="projects/AUR/*/*.tar.bz2" \
     --exclude="projects/AUR/*/*.tar.zst" \
     --exclude=".cache" \
-    --exclude=".nv" \
-    --exclude=".gvfs" \
-    --exclude=".theano" \
-    --exclude=".thumbnails" \
-    --exclude=".wine" \
     --exclude=".local/miniconda3" \
     --exclude=".local/share/Trash" \
     --exclude=".local/share/Steam" \
+    --exclude=".local/share/wine_home" \
+    --exclude=".local/share/cargo" \
     --exclude=".local/share/gvfs-metadata" \
     --exclude=".local/share/recently-used.xbel*" \
     --exclude=".config/chromium" \
     --exclude=".config/Code/*Cache*" \
-    --exclude=".config/Code - OSS/*Cache*" \
-    --exclude=".config/VSCodium/*Cache*" \
+    --exclude=".config/Code/User/workspaceStorage" \
+    --exclude=".config/Code/User/globalStorage" \
     --exclude=".config/Code/logs" \
-    --exclude=".config/Code - OSS/logs" \
-    --exclude=".config/VSCodium/logs" \
-    --exclude=".vscode/extensions" \
-    --exclude=".vscode-oss/extensions" \
+    --exclude=".vscode" \
     ~ "$BACKUP_LOC"
 
-for target in /etc /boot/loader /mnt/win_data; do
+for target in /etc /boot/loader /var/lib/iwd /var/log/pacman.log ; do
     if [[ -d "$target" ]]; then
         rsync -ahv --delete --ignore-errors --delete-excluded "$target" "$BACKUP_LOC"
     fi
