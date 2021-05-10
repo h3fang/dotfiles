0babf9f28ef7265788a8ca13d8f4600b52944ba1
Fang He <hf.enigma@gmail.com>
2021-05-10 14:10:52 +0800

    fix microphone and webcam in qemu Windows10 guest

--- scripts/qemu-win10.sh
+++ scripts/qemu-win10.sh
@@ -9,30 +9,29 @@ function create_disk {
 }
 
 function install {
-    qemu-system-x86_64 -m 4G -smp 4 -enable-kvm \
+    qemu-system-x86_64 -m 4G -smp 8 -enable-kvm \
     -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time \
     -drive file="${DISK_IMG}",index=0,media=disk,if=virtio \
     -nic user,model=virtio,hostfwd=tcp::10022-:22 \
     -rtc base=localtime,clock=host \
-    -audiodev pa,id=pa0,server=/run/user/$UID/pulse/native \
-    -device intel-hda -device hda-duplex,audiodev=pa0 \
+    -audiodev pa,id=snd0 \
+    -device ich9-intel-hda -device hda-micro,audiodev=snd0 \
     -vga virtio \
     -drive file="${WIN10_IMG}",index=2,media=cdrom \
     -drive file="${VIRTIO_IMG}",index=3,media=cdrom
 }
 
 function run {
-    qemu-system-x86_64 -m 4G -smp 4 -enable-kvm \
+    qemu-system-x86_64 -m 4G -smp $(nproc) -enable-kvm \
     -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time \
     -drive file="${DISK_IMG}",if=virtio \
     -nic user,model=virtio-net-pci,hostfwd=tcp::10022-:22 \
     -rtc base=localtime,clock=host \
-    -audiodev pa,id=pa0,server=/run/user/$UID/pulse/native \
-    -device intel-hda -device hda-duplex,audiodev=pa0 \
-    -device usb-ehci,id=ehci -device usb-host,bus=ehci.0,hostbus=1,hostaddr=3 \
+    -audiodev pa,id=snd0 \
+    -device ich9-intel-hda -device hda-micro,audiodev=snd0 \
+    -device usb-ehci,id=ehci -device usb-host,bus=ehci.0,vendorid=0x04f2,productid=0xb6cb \
     -display gtk,gl=on \
-    -vga virtio \
-    -full-screen &
+    -vga virtio &
 }
 
 
