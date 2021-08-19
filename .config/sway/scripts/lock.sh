03d98b370dcab8469405d4746602491d50706ce3
Fang He <hf.enigma@gmail.com>
2021-08-19 09:38:44 +0800

    replace chromium with firefox in ~/.config/sway/scripts/lock.sh

--- .config/sway/scripts/lock.sh
+++ .config/sway/scripts/lock.sh
@@ -2,10 +2,10 @@
 # requires swaylock-effects, sway (for swaymsg), jq (json parsing), pulseaudio (for pacmd)
 
 if [[ $1 != "force" ]]; then
-    chromium_focused=$(swaymsg -t get_tree | jq -r '.. | select(.window_properties?.class == "Chromium") | .focused' | grep "true" | wc -l)
-    chromium_playing=$(pacmd list-sink-inputs | grep 'application.name = "Chromium"' | wc -l)
-    if [[ $chromium_focused -gt 0 && $chromium_playing -gt 0 ]]; then
-        echo "Inhibit idle while chromium is focused and playing"
+    firefox_focused=$(swaymsg -t get_tree | jq -r '.. | select(.app_id? == "firefox") | .focused' | grep "true" | wc -l)
+    firefox_playing=$(pacmd list-sink-inputs | grep 'application.name = "Firefox"' | wc -l)
+    if [[ $firefox_focused -gt 0 && $firefox_playing -gt 0 ]]; then
+        echo "Inhibit idle while Firefox is focused and playing."
         exit 1
     fi
 fi
