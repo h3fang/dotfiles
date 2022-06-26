3eeb49bd8708640700666d5cfba2829a978816f5
Fang He <hf.enigma@gmail.com>
2022-06-26 22:34:58 +0800

    switch back to swaylock-effects

--- .config/sway/scripts/lock.sh
+++ .config/sway/scripts/lock.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
-# requires swaylock-effects, sway (for swaymsg), jq (json parsing), pulseaudio (for pacmd)
-
-if [[ $1 != "force" ]]; then
-    firefox_focused=$(swaymsg -t get_tree | jq -r '.. | select(.app_id? == "firefox") | .focused' | grep "true" | wc -l)
-    firefox_playing=$(pacmd list-sink-inputs | grep 'application.name = "Firefox"' | wc -l)
-    if [[ $firefox_focused -gt 0 && $firefox_playing -gt 0 ]]; then
-        echo "Inhibit idle while Firefox is focused and playing."
-        exit 1
-    fi
-fi
+# requires swaylock-effects, dunst (dunstctl)
 
 dunstctl set-paused true
 
-~/.config/sway/scripts/swaylock-blur -fel --indicator-radius 150 --ring-color=2075c7ff --font monospace --font-size 32
+swaylock_args="-fel --indicator-radius 150 --ring-color 2075c7ff --font monospace --font-size 32"
+# ~/.config/sway/scripts/swaylock-blur ${swaylock_args}
+swaylock ${swaylock_args} --indicator --screenshots --clock --datestr "%a %F" --effect-scale 0.5 --effect-blur 32x3 --effect-scale 2
 
 dunstctl set-paused false
