254512d4a5ccc33d6b93295bcb77cd3f5ace4416
Fang He <hf.enigma@gmail.com>
2022-08-29 10:54:24 +0800

    update clean bash histroy script

--- scripts/clean_bash_history.sh
+++ scripts/clean_bash_history.sh
@@ -8,10 +8,10 @@ sed -Ei '/^(git|dot) (add|status|diff|branch|br|st|di)\b/d' ~/.bash_history
 sed -Ei '/PYTHONIOENCODING=UTF-8/d' ~/.bash_history
 
 # delete pacman commands
-sed -Ei '/^(sudo )?(pacman|yay|aurman|pikaur|pacaur) /d' ~/.bash_history
+sed -Ei '/^(sudo )?(pacman|yay) /d' ~/.bash_history
 
 # delete cd, rm, man, ls, etc.
-sed -Ei '/^(sudo )?(cd|rm|ls|ll|exa|tree|cat|bat|man|which|[pfe]?grep|cp|mv|fd|find|pip)( |$)/d' ~/.bash_history
+sed -Ei '/^(sudo )?(cd|rm|ls|ll|exa|tree|cat|bat|man|which|[pfe]?grep|cp|mv|fd|find|pip|ncdu|ping|pactree)( |$)/d' ~/.bash_history
 
 # curl, wget, youtube-dl
 sed -Ei '/^(sudo )?(curl|wget|youtube-dl) /d' ~/.bash_history
@@ -19,8 +19,8 @@ sed -Ei '/^(sudo )?(curl|wget|youtube-dl) /d' ~/.bash_history
 # delete vim commands
 sed -Ei '/^(sudo )?(vim|nvim) /d' ~/.bash_history
 
-# delete cargo test subcommands
-sed -Ei '/^cargo t /d' ~/.bash_history
+# delete cargo commands
+sed -Ei '/^cargo (t|test|r|run|new) /d' ~/.bash_history
 
 # list top 10 commands
 awk '{print $1}' ~/.bash_history | sort | uniq -c | sort -n | tail -10
