c85de7eab825fd0e5569f1258e6c8467daf215a2
Fang He <hf.enigma@gmail.com>
2022-10-21 20:05:46 +0800

    update printer script

--- scripts/printer.sh
+++ scripts/printer.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-sudo pacman -S cups hplip
-sudo systemctl enable --now org.cups.cupsd.service
-
-echo 'hplip is for HP printers, see https://wiki.archlinux.org/index.php/CUPS/Printer-specific_problems'
+sudo pacman -S cups # hplip hplip-plugin
+sudo systemctl enable cups.socket
 
 echo
 echo 'setup printers'
 echo 'open http://localhost:631/ in web browser'
-echo '1) add current user to groups with printer administration privileges defined in SystemGroup in the /etc/cups/cups-files.conf or 2) login as root'
+echo '1) add current user to groups with printer administration privileges defined in SystemGroup in the /etc/cups/cups-files.conf and login as current user or 2) login as root'
 echo 'add printer'
+echo 'hplip is for HP printers, see https://wiki.archlinux.org/index.php/CUPS/Printer-specific_problems'
 echo 'print'
 
