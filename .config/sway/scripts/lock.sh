c964a2790235b1646e0b2cedfd1a50f119272a2b
Fang He <hf.enigma@gmail.com>
2023-01-07 21:26:19 +0800

    remove swaylock-effects

--- .config/sway/scripts/lock.sh
+++ .config/sway/scripts/lock.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-# requires swaylock-effects, dunst (dunstctl)
+# requires swaylock, dunst (dunstctl)
 
 dunstctl set-paused true
 
-swaylock_args=(-fel --indicator-radius 150 --ring-color 2075c7ff --font monospace --font-size 32)
-# ~/.config/sway/scripts/swaylock-blur "${swaylock_args[@]}"
-swaylock "${swaylock_args[@]}" --indicator --screenshots --clock --datestr "%a %F" --effect-scale 0.5 --effect-blur 32x3 --effect-scale 2
+swaylock -fel --indicator-radius 150 --ring-color 2075c7ff --font monospace --font-size 32 -i ~/Pictures/wallpapers/current
 
 dunstctl set-paused false
