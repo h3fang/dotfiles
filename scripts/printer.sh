f4290fff8ec5555606b42fd2586b0b78605ccff8
Fang He <hf.enigma@gmail.com>
2023-03-08 14:41:22 +0800

    update arch wiki link

--- scripts/printer.sh
+++ scripts/printer.sh
@@ -8,6 +8,6 @@ echo 'setup printers'
 echo 'open http://localhost:631/ in web browser'
 echo '1) add current user to groups with printer administration privileges defined in SystemGroup in the /etc/cups/cups-files.conf and login as current user or 2) login as root'
 echo 'add printer'
-echo 'hplip is for HP printers, see https://wiki.archlinux.org/index.php/CUPS/Printer-specific_problems'
+echo 'hplip is for HP printers, see https://wiki.archlinux.org/title/CUPS/Printer-specific_problems'
 echo 'print'
 
