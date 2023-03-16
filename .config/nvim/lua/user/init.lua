6ead71aaa6e2be8d5117d780d2befa6cc6ae36f6
Fang He <hf.enigma@gmail.com>
2023-03-16 14:09:56 +0800

    update astronvim to 3.3.0

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -4,89 +4,94 @@ local config = {
   },
   -- Extend LSP configuration
   lsp = {
+    formatting = {
+      -- control auto formatting on save
+      format_on_save = {
+        enabled = false,
+      },
+    },
     -- enable servers that you already have installed without mason
     servers = {
       "pyright",
       "rust_analyzer",
     },
   },
-
   -- Configure plugins
   plugins = {
-    init = {
-      ['ethanholz/nvim-lastplace'] = {
-        config = function()
-          require'nvim-lastplace'.setup {
-            lastplace_ignore_buftype = {"quickfix", "nofile", "help"},
-            lastplace_ignore_filetype = {"gitcommit", "gitrebase", "svn", "hgcommit"},
-            lastplace_open_folds = true
-          }
-        end,
-      },
-      ['saecki/crates.nvim'] = {
-        event = { "BufRead Cargo.toml" },
-        requires = { { 'nvim-lua/plenary.nvim' } },
-        config = function()
-          require('crates').setup()
-        end,
+    {
+      "williamboman/mason-lspconfig.nvim",
+      opts = {
+        ensure_installed = {
+          "lua_ls",
+        },
       },
     },
-    -- All other entries override the require("<key>").setup({...}) call for default plugins
-    ["null-ls"] = function(config) -- overrides `require("null-ls").setup(config)`
-      -- config variable is the default configuration table for the setup function call
-      local null_ls = require "null-ls"
+    {
+      "jay-babu/mason-null-ls.nvim",
+      config = function()
+        require("mason-null-ls").setup {
+          ensure_installed = { "stylua" },
+        }
+      end,
+    },
+    {
+      "jose-elias-alvarez/null-ls.nvim",
+      opts = function(_, config)
+        local null_ls = require "null-ls"
+        config.sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.black,
+          null_ls.builtins.formatting.rustfmt,
+          null_ls.builtins.formatting.clang_format,
+        }
+        return config
+      end,
+    },
+    {
+      "nvim-treesitter/nvim-treesitter",
+      opts = {
+        ensure_installed = { "lua", "rust", "python", "c", "cpp" },
+      },
+    },
+    {
+      "ethanholz/nvim-lastplace",
+      config = function()
+        require("nvim-lastplace").setup {
+          lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+          lastplace_ignore_filetype = { "gitcommit", "gitrebase", "svn", "hgcommit", "alpha", "mason", "toggleterm", "lazy" },
+          lastplace_open_folds = true,
+        }
+      end,
+    },
 
-      -- Check supported formatters and linters
-      -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-      -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-      config.sources = {
-        null_ls.builtins.formatting.rustfmt,
-        null_ls.builtins.formatting.black,
-      }
-      return config -- return final config table
-    end,
-    treesitter = { -- overrides `require("treesitter").setup(...)`
-      ensure_installed = { "rust", "python" },
+    {
+      "johnfrankmorgan/whitespace.nvim",
+      config = function()
+        require("whitespace-nvim").setup {
+          highlight = "DiffDelete",
+          ignored_filetypes = { "TelescopePrompt", "Trouble", "help", "alpha", "mason", "toggleterm", "lazy" },
+        }
+      end,
+      lazy = false,
     },
-    -- use mason-lspconfig to configure LSP installations
-    ["mason-lspconfig"] = { -- overrides `require("mason-lspconfig").setup(...)`
-      -- ensure_installed = { "sumneko_lua" },
+    {
+      "L3MON4D3/LuaSnip",
+      config = function(plugin, opts)
+        require "plugins.configs.luasnip" (plugin, opts)
+        require("luasnip.loaders.from_vscode").lazy_load { paths = { "./lua/user/snippets" } }
+      end,
     },
-    -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
-    ["mason-null-ls"] = { -- overrides `require("mason-null-ls").setup(...)`
-      -- ensure_installed = { "black", "rustfmt" },
-    },
-    ["mason-nvim-dap"] = { -- overrides `require("mason-nvim-dap").setup(...)`
-      -- ensure_installed = { "python" },
+    {
+      "saecki/crates.nvim",
+      event = { "BufRead Cargo.toml" },
+      requires = { { "nvim-lua/plenary.nvim" } },
+      config = function() require("crates").setup() end,
     },
   },
-
-  -- LuaSnip Options
-  luasnip = {
-    -- Extend filetypes
-    filetype_extend = {
-      -- javascript = { "javascriptreact" },
-    },
-    -- Configure luasnip loaders (vscode, lua, and/or snipmate)
-    vscode = {
-      -- Add paths for including more VS Code style snippets in luasnip
-      paths = {},
-    },
-  },
-
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
   polish = function()
-    -- highlight trailing white spaces
-    -- https://vim.fandom.com/wiki/Highlight_unwanted_spaces#Highlighting_with_the_match_command
-    vim.cmd([[
-      hi ExtraWhitespace ctermbg=red guibg=red
-      match ExtraWhitespace /\s\+$/
-      autocmd InsertEnter * match ExtraWhitespace /\s\+\%#\@<!$/
-      autocmd InsertLeave * match ExtraWhitespace /\s\+$/
-      autocmd BufWinLeave * call clearmatches()
-    ]])
   end,
 }
 return config
