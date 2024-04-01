21804da18e150f9da44c20b97d08a9bdea8b7d36
Fang He <hf.enigma@gmail.com>
2024-04-01 15:45:15 +0800

    disable wezterm key bindings with SUPER

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,11 +1,11 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.font = wezterm.font_with_fallback{
-  'Hack Nerd Font',
-  'Noto Sans Symbols 2',
-  'monospace',
-}
+config.font = wezterm.font_with_fallback({
+  "Hack Nerd Font",
+  "Noto Sans Symbols 2",
+  "monospace",
+})
 
 config.font_size = 13.0
 config.window_background_opacity = 0.90
@@ -15,48 +15,58 @@ config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.use_dead_keys = false
 
+config.disable_default_key_bindings = true
+config.keys = {}
+
+local keys = require("default_key_bindings").keys
+for _, v in ipairs(keys) do
+  if string.find(v.mods, "SUPER") == nil then
+    table.insert(config.keys, v)
+  end
+end
+
 config.colors = {
   -- The default text color
-  foreground = 'white',
+  foreground = "white",
   -- The default background color
-  background = 'black',
+  background = "black",
 
   -- Overrides the cell background color when the current cell is occupied by the
   -- cursor and the cursor style is set to Block
-  cursor_bg = '#1a8fff',
+  cursor_bg = "#1a8fff",
   -- Overrides the text color when the current cell is occupied by the cursor
-  cursor_fg = 'black',
+  cursor_fg = "black",
 
   -- the foreground color of selected text
-  selection_fg = 'black',
+  selection_fg = "black",
   -- the background color of selected text
-  selection_bg = '#affacd',
+  selection_bg = "#affacd",
 
   -- The color of the scrollbar "thumb"; the portion that represents the current viewport
-  scrollbar_thumb = '#666',
+  scrollbar_thumb = "#666",
 
   -- The color of the split lines between panes
-  split = '#666',
+  split = "#666",
 
   ansi = {
-    'black',
-    '#B61C2B',
-    '#00AA00',
-    '#AA5500',
-    '#0952EB',
-    '#BB08BB',
-    '#07BBBB',
-    '#AAAAAA',
+    "black",
+    "#B61C2B",
+    "#00AA00",
+    "#AA5500",
+    "#0952EB",
+    "#BB08BB",
+    "#07BBBB",
+    "#AAAAAA",
   },
   brights = {
-    '#555555',
-    '#ff5555',
-    '#55FF55',
-    '#FFFF55',
-    '#1a8fff',
-    '#FF55FF',
-    '#55FFFF',
-    'white',
+    "#555555",
+    "#ff5555",
+    "#55FF55",
+    "#FFFF55",
+    "#1a8fff",
+    "#FF55FF",
+    "#55FFFF",
+    "white",
   },
 }
 
