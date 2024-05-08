4ab3d9814bb7e534ab6276066749053ccecfd8a8
Fang He <hf.enigma@gmail.com>
2024-05-08 22:07:39 +0800

    use umount instead of udiskie-umount

--- scripts/reformat_usb_disk.sh
+++ scripts/reformat_usb_disk.sh
@@ -14,10 +14,10 @@ lsblk -f /dev/"${usb_disk}"
 read -p "Continue to reformat /dev/${usb_disk}? (yes|[no])" -r
 if [[ $REPLY == "yes" ]]
 then
-    udiskie-umount /dev/"${usb_disk}"1 || true
+    # sudo umount /dev/"${usb_disk}"1 || true
     sudo wipefs --all /dev/"${usb_disk}"
     sudo parted -s /dev/"${usb_disk}" mklabel msdos mkpart primary fat32 0% 100%
-    udiskie-umount /dev/"${usb_disk}"1 || true
+    sudo umount /dev/"${usb_disk}"1 || true
     sudo mkfs.fat -F 32 -n HEF /dev/"${usb_disk}"1
 fi
 
