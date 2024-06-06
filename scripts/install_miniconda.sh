66f5541fec245f3f425125fad9ca3d8b7be7d553
Fang He <hf.enigma@gmail.com>
2024-06-06 14:41:56 +0800

    remove miniconda mirrors

--- scripts/install_miniconda.sh
+++ scripts/install_miniconda.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-MINICONDA_PATH=~/.local/miniconda3
-INSTALLER_URL="https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+MINICONDA_PATH=~/.local/share/miniconda3
+INSTALLER_URL="https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
 
 if [[ -d "$MINICONDA_PATH" ]]; then
     read -p "found existing ~/.local/miniconda3, remove it? y/[n]" -r
@@ -14,12 +14,6 @@ fi
 read -p "install the latest miniconda? (y/[n]) " -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    read -p "use installer from tsinghua mirror? (y/[n]) " -r
-    echo
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        INSTALLER_URL="https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-x86_64.sh"
-    fi
-
     # silent install to $MINICONDA_PATH
     MINICONDA_FILE=$(mktemp)
     curl -Lo "$MINICONDA_FILE" "$INSTALLER_URL"
@@ -31,16 +25,6 @@ fi
 source "${MINICONDA_PATH}/bin/activate"
 
 conda config --system --set changeps1 false
-
-read -p "use tsinghua anaconda mirror? (y/[n]) " -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    conda config --system --set show_channel_urls yes
-    conda config --system --add channels defaults
-    conda config --system --add default_channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main
-    conda config --system --set custom_channels.pytorch https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
-    conda config --system --set custom_channels.conda-forge https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
-fi
 
 # update base
 conda update conda
