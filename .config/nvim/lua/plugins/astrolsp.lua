71ec280292686ef31bc66939e76dc70f5aba69e0
Fang He <hf.enigma@gmail.com>
2024-10-01 10:48:04 +0800

    safely extend astronvim servers list

--- .config/nvim/lua/plugins/astrolsp.lua
+++ .config/nvim/lua/plugins/astrolsp.lua
@@ -1,14 +1,15 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrolsp",
-  ---@type AstroLSPOpts
-  opts = {
-    features = {
-      inlay_hints = true,
-    },
-    formatting = {
-      format_on_save = false,
-    },
-    servers = { "rust_analyzer", "pyright" },
-  },
+  ---@param opts AstroLSPOpts
+  opts = function(_, opts)
+    opts.features.inlay_hints = true
+    opts.formatting.format_on_save = false
+    -- safely extend the servers list
+    opts.servers = opts.servers or {}
+    vim.list_extend(opts.servers, {
+      "rust_analyzer",
+      "pyright",
+    })
+  end,
 }
