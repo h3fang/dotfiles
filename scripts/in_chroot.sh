728a372028a14e9c6bb629cb3d9260d1b5bf7bc5
Fang He <hf.enigma@gmail.com>
2025-06-26 23:36:49 +0800

    fix mistakes and update scripts/in_chroot.sh

--- scripts/in_chroot.sh
+++ scripts/in_chroot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #########
-### assuming /dev/sda2 is root, Asia/Shanghai timezone, en_US and zh_CN locale, vanilla linux kernel, bash for created user
+### assuming Asia/Shanghai timezone, en_US and zh_CN locale, linux-zen kernel, zsh for created user
 #########
 
 set -eEuo pipefail
@@ -10,37 +10,35 @@ failure() {
 }
 trap 'failure ${LINENO} "$BASH_COMMAND" $?' ERR
 
-HostName=h3f-arch
+HostName=arch
 UserName=h3f
-RootUUID=$(blkid /dev/sda2 | awk '{print $5}')
+RootUUID=$(blkid /dev/nvme0n1p2 | awk '{print $5}')
 
-pacman -S base-devel git
+pacman -S --needed base-devel git git-delta git-lfs zsh zsh-syntax-highlighting zsh-autosuggestions zsh-completions neovim
 
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 hwclock --systohc
 
 sed -i '/^#en_US.UTF-8 UTF-8/ s/^#//' /etc/locale.gen
-sed -i '/^#zh_CN.GB18030 GB18030/ s/^#//' /etc/locale.gen
 sed -i '/^#zh_CN.UTF-8 UTF-8/ s/^#//' /etc/locale.gen
 locale-gen
 echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 echo 'LC_PAPER=zh_CN.UTF-8' >> /etc/locale.conf
 echo 'A4' >> /etc/papersize
 
-cat > /etc/hostname <<EOF
-$HostName
+echo "$HostName" > /etc/hostname
+cat > /etc/hosts <<EOF
 127.0.0.1 localhost
 ::1 localhost
-127.0.1.1 ${HostName}.local ${HostName}
 EOF
 
 mkinitcpio -p linux
 echo "setting root password..."
 passwd root
-useradd -m -G wheel -s /bin/bash $UserName
+useradd -m -G wheel -s /usr/bin/zsh $UserName
 echo "setting $UserName password..."
 passwd $UserName
-echo "$UserName ALL=(ALL) ALL" >> /etc/sudoers
+echo "$UserName ALL=(ALL:ALL) ALL" >> /etc/sudoers.d/$UserName
 
 Microcode=""
 echo 'Microcode?'
@@ -51,10 +49,9 @@ select ucode in "amd" "intel"; do
     esac
 done
 
-function bl_systemd_boot {
-    bootctl --path=/boot install
+bootctl --path=/boot install
 
-    cat > /etc/pacman.d/hooks/100-systemd-boot.hook <<EOF
+cat > /etc/pacman.d/hooks/100-systemd-boot.hook <<EOF
 [Trigger]
 Type = Package
 Operation = Upgrade
@@ -66,34 +63,17 @@ When = PostTransaction
 Exec = /usr/bin/bootctl update
 EOF
 
-    cat > /boot/loader/loader.conf <<EOF
+cat > /boot/loader/loader.conf <<EOF
 timeout 3
 default arch
 editor no
 EOF
 
-    cat > /boot/loader/entries/arch.conf <<EOF
-title ArchLinux
-linux /vmlinuz-linux
+cat > /boot/loader/entries/arch.conf <<EOF
+title Arch Linux
+linux /vmlinuz-linux-zen
 initrd /${Microcode}.img
-initrd /initramfs-linux.img
-options root=$RootUUID rw quiet nmi_watchdog=0 audit=0 module.sig_enforce=1
+initrd /initramfs-linux-zen.img
+options root=$RootUUID rw quiet nmi_watchdog=0 audit=0
 EOF
-}
 
-echo 'Boot loader?'
-select bl in "grub-mbr" "grub-uefi" "systemd-boot"; do
-    case $bl in
-        grub-mbr )
-            pacman -S grub ntfs-3g os-prober
-            grub-install --target=i386-pc /dev/sda
-            grub-mkconfig -o /boot/grub/grub.cfg
-            break;;
-        grub-uefi )
-            pacman -S grub ntfs-3g os-prober
-            grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-            grub-mkconfig -o /boot/grub/grub.cfg
-            break;;
-        systemd-boot ) bl_systemd_boot; break;;
-    esac
-done
